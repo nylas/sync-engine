6aa0315d5b639676efb072661ee6dfc462b55bcd
Michael Grinich <mg@inboxapp.com>
2013-04-11 16:15:59 -0700

    Remove my oauth bits :)

---
+++ auth_sample.py
@@ -0,0 +1,12 @@
+import oauth2 as oauth
+
+BASE_GMAIL_URL = 'https://mail.google.com/mail/b/' + account + '/imap/'
+HOST = 'imap.gmail.com'
+
+OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN'
+OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET
+
+CONSUMER_KEY = 'anonymous'
+CONSUMER_SECRET = 'anonymous'
+
+ACCOUNT = 'YOUR_EMAIL@gmail.com'
