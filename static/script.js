0c02dc0db5956f045683c8a51cd4487119ee0355
Michael Grinich <mg@inboxapp.com>
2013-04-13 11:14:42 -0700

    syntax formatting

--- static/script.js
+++ static/script.js
@@ -3,12 +3,10 @@
 function autoResize(id){
     var newheight;
     var newwidth;
-
     if(document.getElementById){
         newheight=document.getElementById(id).contentWindow.document .body.scrollHeight;
         newwidth=document.getElementById(id).contentWindow.document .body.scrollWidth;
     }
-
     document.getElementById(id).height= (newheight) + "px";
     document.getElementById(id).width= (newwidth) + "px";
 }
