2fac66b0923b306432e6e57f73995523ed40d915
Michael Grinich <mg@inboxapp.com>
2013-04-13 12:45:14 -0700

    Add note on how to make git ignore changes to credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,5 @@
+# You should run this so git doesn't bother you when it's been changed.
+# git update-index --assume-unchanged credentials.py
 ACCOUNT = 'YOUR_EMAIL@gmail.com'
 OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN'
 OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET'
