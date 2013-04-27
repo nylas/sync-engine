4b4e004a1193483be8736dda3f2c4900b6bde6c6
Michael Grinich <mg@inboxapp.com>
2013-04-27 16:48:24 -0700

    Better message for generating oauth

--- credentials.py
+++ credentials.py
@@ -1,5 +1,10 @@
 # You should run this so git doesn't bother you when it's been changed.
-# git update-index --assume-unchanged credentials.py
+# $ git update-index --assume-unchanged credentials.py
+
+# To make credentials, run
+# $ python xoauth.py --generate_oauth_token --user=YOUR_EMAIL@gmail.com
+# visit the link, accept auth, and enter the verification code
+
 ACCOUNT = 'YOUR_EMAIL@gmail.com'
 OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN'
 OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET'
