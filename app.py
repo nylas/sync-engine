4abc5795457193d5cfb9f06592b4b5db5bc95f82
Michael Grinich <mg@inboxapp.com>
2013-04-30 18:08:40 -0700

    Adding welcome page. Let's do more angular!

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ define("port", default=8888, help="run on the given port", type=int)
 import os.path as os_path
 import logging as log
 
+# static request handler
+import os
+import datetime
+import stat
+import mimetypes
+
 from crispin import CrispinClient
 
 crispin_client = None
@@ -25,18 +31,20 @@ class Application(tornado.web.Application):
         )
 
         handlers = [
-            (r"/", MailboxHandler),
+            (r'/app/(.*)', AppStaticFileHandler),
             (r"/mailbox", MailboxHandler),
             (r"/mailbox_json", MailboxJSONHandler),
             (r"/thread", MessageThreadHandler),
-            (r'/app/(.*)', tornado.web.StaticFileHandler, {'path': os_path.join(os_path.dirname(__file__), "angular")}),
             (r"/message", MessagePageHandler),
             (r"/message_raw", MessageRawHandler),
             # (r"/(apple-touch-icon\.png)", tornado.web.StaticFileHandler,
             # (r"/(apple-touch-icon\.png)", tornado.web.StaticFileHandler,
+            (r'/(.*)', tornado.web.StaticFileHandler, {'path': os_path.join(os_path.dirname(__file__), "static"), 'default_filename':'index.html'}),
         ]
 
         tornado.web.Application.__init__(self, handlers, **settings)
+
+
 
 class BaseHandler(tornado.web.RequestHandler):
     pass
@@ -153,6 +161,97 @@ class MessageThreadHandler(BaseHandler):
             thread_ids = msg_ids)
 
         
+
+
+class AppStaticFileHandler(tornado.web.RequestHandler):
+    """A simple handler that can serve static content from a directory.
+ 
+    To map a path to this handler for a static data directory /var/www,
+    you would add a line to your application like:
+ 
+        application = web.Application([
+            (r"/static/(.*)", web.StaticFileHandler, {"path": "/var/www"}),
+        ])
+ 
+    The local root directory of the content should be passed as the "path"
+    argument to the handler.
+ 
+    To support aggressive browser caching, if the argument "v" is given
+    with the path, we set an infinite HTTP expiration header. So, if you
+    want browsers to cache a file indefinitely, send them to, e.g.,
+    /static/images/myimage.png?v=xxx.
+    """
+    def initialize(**kwargs):
+        
+        path = os_path.join(os_path.dirname(__file__), "angular")
+        default_filename = 'index.html'
+        self.root = os.path.abspath(path) + os.path.sep
+        self.default_filename = default_filename
+ 
+    def head(self, path):
+        self.get(path, include_body=False)
+ 
+    def get(self, path, include_body=True):
+        if os.path.sep != "/":
+            path = path.replace("/", os.path.sep)
+        abspath = os.path.abspath(os.path.join(self.root, path))
+        # os.path.abspath strips a trailing /
+        # it needs to be temporarily added back for requests to root/
+        if not (abspath + os.path.sep).startswith(self.root):
+            raise HTTPError(403, "%s is not in root static directory", path)
+        if os.path.isdir(abspath) and self.default_filename is not None:
+            # need to look at the request.path here for when path is empty
+            # but there is some prefix to the path that was already
+            # trimmed by the routing
+            if not self.request.path.endswith("/"):
+                self.redirect(self.request.path + "/")
+                return
+            abspath = os.path.join(abspath, self.default_filename)
+        if not os.path.exists(abspath):
+            raise HTTPError(404)
+        if not os.path.isfile(abspath):
+            raise HTTPError(403, "%s is not a file", path)
+ 
+        stat_result = os.stat(abspath)
+        modified = datetime.datetime.fromtimestamp(stat_result[stat.ST_MTIME])
+ 
+        self.set_header("Last-Modified", modified)
+        if "v" in self.request.arguments:
+            self.set_header("Expires", datetime.datetime.utcnow() + \
+                                       datetime.timedelta(days=365*10))
+            self.set_header("Cache-Control", "max-age=" + str(86400*365*10))
+        else:
+            self.set_header("Cache-Control", "public")
+        mime_type, encoding = mimetypes.guess_type(abspath)
+        if mime_type:
+            self.set_header("Content-Type", mime_type)
+ 
+        self.set_extra_headers(path)
+ 
+        # Check the If-Modified-Since, and don't send the result if the
+        # content has not been modified
+        ims_value = self.request.headers.get("If-Modified-Since")
+        if ims_value is not None:
+            date_tuple = email.utils.parsedate(ims_value)
+            if_since = datetime.datetime.fromtimestamp(time.mktime(date_tuple))
+            if if_since >= modified:
+                self.set_status(304)
+                return
+ 
+        if not include_body:
+            return
+        file = open(abspath, "rb")
+        try:
+            self.write(file.read())
+        finally:
+            file.close()
+ 
+    def set_extra_headers(self, path):
+        """For subclass to add extra headers to the response"""
+        pass
+
+
+
 
 
 def main():
