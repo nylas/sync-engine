ec4d38aeedc94db5afce0d0c84e27b0a0f3fc768
Michael Grinich <mg@inboxapp.com>
2013-07-14 16:34:43 -0700

    move keys to secrets.py

---
+++ server/secrets.py
@@ -0,0 +1,11 @@
+
+# shh
+
+COOKIE_SECRET = "32oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
+
+
+# You can manage (ie: reset) these in the Google API Console at https://code.google.com/apis/console
+# The account is admin@inboxapp.com
+
+GOOGLE_CONSUMER_KEY = "786647191490.apps.googleusercontent.com"
+GOOGLE_CONSUMER_SECRET = "0MnVfEYfFebShe9576RR8MCK"
