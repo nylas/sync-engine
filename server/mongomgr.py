4c493bfc62250e280b374a67cf7dabbf68b6c2d6
Michael Grinich <mg@inboxapp.com>
2013-07-13 22:53:56 -0700

    do the proper thing with mongod

--- server/mongomgr.py
+++ server/mongomgr.py
@@ -2,17 +2,35 @@
 
 import subprocess
 import logging as log
+import os
 
-
-mongod_process = None
+db_path = None
 
 def startmongo(PATH_TO_MONGO_DATABSE):
-    log.info("Starting Mongo")
-    args = ['mongod', '--dbpath', PATH_TO_MONGO_DATABSE]
-    global mongod_process
+    global db_path
+    db_path = PATH_TO_MONGO_DATABSE
+    
+    log.info("Starting Mongo. DB at %s" % PATH_TO_MONGO_DATABSE)
+    if not os.path.exists(PATH_TO_MONGO_DATABSE):
+        os.makedirs(PATH_TO_MONGO_DATABSE)
+    args = ['mongod', '--dbpath', PATH_TO_MONGO_DATABSE, '--fork']
     mongod_process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    mongod_process.communicate()
+
 
 def stopmongo():
-    log.info("Stopping Mongo")
-    global mongod_process
-    mongod_process.terminate()
+
+    global db_path
+    path_to_pid = os.path.join(db_path, 'mongod.lock')
+
+    try:
+        f = open(path_to_pid)
+    except Exception, e:
+        log.info("Error stopping mongo: no PID in mongod.lock")
+        return
+
+    pid = ' '.join(f.read().split())
+    log.info("Stopping Mongo (%s)" % pid)
+    args = ['kill', '-2', pid]
+    mongod_process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    mongod_process.communicate()
