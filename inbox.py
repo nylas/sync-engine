76dab4bcc991854dff5771f6f30be97cfcff9b08
Greg Price <price@mit.edu>
2013-08-05 18:41:20 -0700

    Handle mongod start/stop a bit better

--- inbox.py
+++ inbox.py
@@ -43,7 +43,7 @@ def start_mongo():
     log.info("Starting Mongo. DB at %s" % PATH_TO_MONGO_DATABSE)
     if not os.path.exists(PATH_TO_MONGO_DATABSE):
       os.makedirs(PATH_TO_MONGO_DATABSE)
-    args = ['mongod', '--dbpath', PATH_TO_MONGO_DATABSE, '--fork']
+    args = ['mongod', '--dbpath', PATH_TO_MONGO_DATABSE, '--fork', '--logpath=/tmp/inbox-mongo.log']
     mongod_process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
     mongod_process.communicate()
     sleep(1) # for mongo
@@ -102,7 +102,7 @@ def stop(args):
 
     # Stop mongo
     log.info("Stopping Mongo.")
-    os.system("kill $(ps aux | grep '[m]ongod' | awk '{print $2}')")
+    os.system("pkill mongod")
 
 
 
