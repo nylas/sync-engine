d79681abdf71053d090f822b1c8650ae2f250b92
Christine Spang <spang@inboxapp.com>
2013-08-25 18:32:04 -0700

    Dev script for printing IDLE messages.

---
+++ tools/idle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys, os;  sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'server')))
+
+import sessionmanager
+
+c = None
+
+def refresh_crispin():
+    global c
+    c = sessionmanager.get_crispin_from_email(
+                    'christine.spang@gmail.com')
+
+refresh_crispin()
+
+folder = c.all_mail_folder_name()
+select_info = c.select_folder(folder)
+
+c.imap_server.idle()
+
+print "Waiting for IDLE messages on {0}...".format(folder)
+
+while True:
+    for msg in c.imap_server.idle_check():
+        print msg
