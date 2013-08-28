70d417d1c8b179a95400d6dc070d8da318edd343
Christine Spang <spang@inboxapp.com>
2013-08-27 19:46:52 -0700

    encoding.py: Fix creation of errors dir.

--- server/encoding.py
+++ server/encoding.py
@@ -12,6 +12,8 @@ from email.header import decode_header
 import logging as log
 import more_codecs
 
+import os
+
 
 import more_codecs  # Go look at it...
 
@@ -603,21 +605,20 @@ def attempt_decoding(charset, dec):
         log.error("Charset lookup error for %s -- %s" % (charset, e))
 
         # payload_data.encode('utf-8')
-        directory ='../errors/'
-        try: os.makedirs(directory)  # TOFIX broken
-        except: pass
+        directory = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                '..', 'errors')
+        # XXX remove this race condition in python 3.2+ using 'exist_ok=True'
+        if not os.path.exists(directory): os.makedirs(directory)
         from hashlib import sha256
         filename = charset + '-' + sha256(dec).hexdigest() + '.txt'
 
         log.error("Writing to file..." + directory+filename)
 
-        f = open(directory+filename, 'w')
+        f = open(os.path.join(directory,filename), 'w')
         f.write(dec)
         f.close()
 
         return guess_encoding_and_decode(charset, dec)
-
-
 
 def apply_charset_to_header(charset, encoding, data):
     if encoding == 'b' or encoding == 'B':
