8f6b19bacf3e79e82c2799e84eeac9b82758b313
Michael Grinich <mg@inboxapp.com>
2013-09-05 13:49:41 -0700

    better confguration options. create a file config.cfg in the root directory. see config-sample.cfg for an example

---
+++ config.py
@@ -0,0 +1,17 @@
+import ConfigParser
+from os import environ
+import logging as log
+
+def setup_env():
+	try:
+	    """Set environment variables from config file"""
+	    config_filename = 'config.cfg'
+	    parser=ConfigParser.SafeConfigParser()
+	    parser.read([config_filename])
+	    for item in parser.items('inboxserver'):
+	        k,v = item[0].upper(), item[1]  # All env vars uppercase
+	        environ[k] = v
+	    log.info("Loaded configuration from %s" % config_filename)
+	except Exception, e:
+		log.error("Error loading configuration from %s. %e" (config_filename, e))
+		raise e
