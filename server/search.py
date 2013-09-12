ea9527a31152020f7d4282f9a6b35e5c335f5165
Christine Spang <spang@inboxapp.com>
2013-09-11 22:09:02 -0700

    Search fixes.

--- server/search.py
+++ server/search.py
@@ -3,11 +3,18 @@ import xapian
 
 class SearchService:
     """ ZeroRPC interface to searching. """
-    def search(user_email_address, query_string):
+    def search(self, user_email_address, query_string, limit=10):
+        """ returns [(g_msgid, relevancerank, fulltext)] """
+        # treat all searches like wildcard searches unless the wildcard is
+        # used elsewhere
+        # XXX we might also want to let queries _start_ with a * and still
+        # append a * to the end
+        if '*' not in query_string:
+            query_string += '*'
         log.info("Searching for {0} on behalf of {1}".format(query_string,
             user_email_address))
         # Open the database for searching.
-        database = xapian.Database("../parts.db")
+        database = xapian.Database("parts.db")
 
         # Start an enquire session.
         enquire = xapian.Enquire(database)
@@ -20,17 +27,17 @@ class SearchService:
         qp.set_stemming_strategy(xapian.QueryParser.STEM_SOME)
 
         query = qp.parse_query(query_string, xapian.QueryParser.FLAG_WILDCARD)
-        print "Parsed query is: %s" % str(query)
+        log.info("Parsed query is: %s" % str(query))
 
-        # Find the top 10 results for the query.
+        # Find the top N results for the query.
         enquire.set_query(query)
-        matches = enquire.get_mset(0, 10)
+        matches = enquire.get_mset(0, limit)
 
         # Display the results.
-        print "%i results found." % matches.get_matches_estimated()
-        print "Results 1-%i:" % matches.size()
+        log.info("%i results found." % matches.get_matches_estimated())
+        log.info("Results 1-%i:" % matches.size())
 
-        # for m in matches:
-        #     print "%i: %i%% docid=%i [%s]" % (m.rank + 1, m.percent, m.docid, m.document.get_data())
-        # XXX what data structure to return?
-        return matches
+        # XXX I think xapian also allows to apply highlights, can we do that?
+
+        results = [(m.docid, m.rank, m.document.get_data()) for m in matches]
+        return results
