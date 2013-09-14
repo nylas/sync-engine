bc990df08d060d79bf781c129c0adc450d959190
Christine Spang <spang@inboxapp.com>
2013-09-14 08:19:28 -0400

    Wrong ordering now that we have foreign keys

--- tools/clear_db.sql
+++ tools/clear_db.sql
@@ -1,7 +1,7 @@
 drop table if exists attachmentpart;
 drop table if exists foldermeta;
-drop table if exists messagemeta;
 drop table if exists messagepart;
+drop table if exists messagemeta;
 drop table if exists uidvalidity;
 drop table if exists user_sessions;
 drop table if exists users;
