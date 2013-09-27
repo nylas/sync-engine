16956e5599dbe4c2433128ecc5c2dba33d7d5417
Christine Spang <spang@inboxapp.com>
2013-09-27 11:48:54 -0700

    Add a few more tables to these sql scripts.

--- tools/clear_db.sql
+++ tools/clear_db.sql
@@ -1,7 +1,10 @@
+drop table if exists collections;
+drop table if exists collections;
 drop table if exists attachmentpart;
 drop table if exists foldermeta;
 drop table if exists messagepart;
 drop table if exists messagemeta;
 drop table if exists uidvalidity;
+drop table if exists rawmessage;
 drop table if exists user_sessions;
 drop table if exists users;
--- tools/clear_db_minus_users.sql
+++ tools/clear_db_minus_users.sql
@@ -1,6 +1,8 @@
+drop table if exists collections;
 drop table if exists attachmentpart;
 drop table if exists foldermeta;
 drop table if exists messagepart;
 drop table if exists messagemeta;
 drop table if exists uidvalidity;
+drop table if exists rawmessage;
 update users set initial_sync_done = 0;
