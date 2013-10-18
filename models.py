60732ab2570e3f91d0a81a008d3a3470879eb8b0
Michael Grinich <mg@mit.edu>
2013-10-18 12:26:38 -0700

    basic db creation and insertion

---
+++ models.py
@@ -0,0 +1,56 @@
+import os
+
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Enum, Text
+from sqlalchemy import ForeignKey, Table, Index, func
+from sqlalchemy.types import PickleType
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+import logging as log
+
+
+class Contact(Base):
+    """ Inbox-specific sessions. """
+    __tablename__ = 'contact'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    token = Column(String(40))
+    # sessions have a many-to-one relationship with users
+    user_id = Column(Integer, nullable=False)
+
+    @property
+    def email_address(self):
+        return self.user.root_namespace.imapaccount.email_address
+
+
+## Make the tables
+from sqlalchemy import create_engine
+DB_URI = "sqlite:///contacts.db"
+
+engine = create_engine(DB_URI)
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+init_db()
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker()
+Session.configure(bind=engine)
+
+# A single global database session per Inbox instance is good enough for now.
+db_session = Session()
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+
+c = Contact()
+c.token = "hi world"
+c.user_id = 2541
+
+contacts_to_commit = [c]
+
+db_session.add_all(contacts_to_commit)
+db_session.commit()
+
