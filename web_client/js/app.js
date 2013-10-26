f0dc5f9f40102ee6d4e802b569593a231da9743d
Michael Grinich <mg@mit.edu>
2013-10-26 11:23:25 -0700

    Show mail UI and load stuff

--- web_client/js/app.js
+++ web_client/js/app.js
@@ -20,5 +20,4 @@ angular.module('InboxApp.controllers', []);
 angular.module('InboxApp.directives', []);
 
 // Some configuration
-app.constant('WIRE_ENDPOINT_URL', 'https://dev-localhost.inboxapp.com/wire');
-
+app.constant('WIRE_ENDPOINT_URL', '/wire');
