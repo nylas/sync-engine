28ff7849cffab0ee353dc01cee3130a481bc5e8f
Michael Grinich <mg@mit.edu>
2013-10-25 22:21:22 -0700

    A more polished todo list interface. in general, this is less fucked than before. the default view it loads is todo, which only shows mock data.

--- web_client/js/controllers/AppContainerController.js
+++ web_client/js/controllers/AppContainerController.js
@@ -9,80 +9,92 @@ var alert = alert;
 var app = angular.module('InboxApp.controllers');
 
 app.controller('AppContainerController',
-function(
-    $scope,
-    $rootScope,
-    Wire,
-    growl,
-    Layout, // Needed to initialize
-    IBThread,
-    IBMessageMeta,
-    protocolhandler,
-    $filter,
-    $timeout,
-    $log,
-    MockData)
-{
+    function($scope,
+        $rootScope,
+        Wire,
+        growl,
+        Layout, // Needed to initialize
+        IBThread,
+        IBMessageMeta,
+        IBMessagePart,
+        protocolhandler,
+        $filter,
+        $timeout,
+        $log,
+        MockData) {
 
-    // TODO
-    // Mousetrap.bind('command+shift+k', function(e) {
-    //     alert('command+shift+k');
-    //     return false;
-    // });
+        // $scope.notificationButtonClick = function() {
+        //     growl.requestPermission(
+        //         function success() {
+        //             console.log("Enabled notifications");
+        //             growl.post("Updates from Disconnect", "MG: Lorem ipsum dolor sit amet, consectetur adipisicing");
 
-    $scope.threads = []; // For UI element
-    $scope.displayedThreads = []; // currently displayed
+        //         }, function failure() {
+        //             console.log("Failure Enabling notifications");
+        //         }
+        //     );
+        // };
 
-    $scope.message_map = {}; // Actual message cache
-    $scope.activeThread = undefined; // Points to the current active mssage
-    $scope.activeNamespace = undefined;
+        // protocolhandler.register();
 
+        // $rootScope.$on('LocalStorageModule.notification.error', function(e) {
+        //     console.log(e);
+        // })
 
-    $scope.performSearch = function(query) {
-        $log.info("performSearch()");
-    };
-
-    $scope.archiveButtonHandler = function() {
-        $log.info("archiveButtonHandler()");
-    };
-
-    $scope.clearSearch = function() {
-        $log.info("clearSearch()");
-    };
+        // TODO
+        // Mousetrap.bind('command+shift+k', function(e) {
+        //     alert('command+shift+k');
+        //     return false;
+        // });
 
 
+        $scope.threads = []; // For UI element
 
-    $scope.loadNamespaces = function()  {
-        Wire.rpc('top_level_namespaces', [], function(data) {
-            var parsed = JSON.parse(data);
-            $log.info(parsed);
+        $scope.displayedThreads = []; // currently displayed
 
-            var gmail_namespace = parsed.private[0];
-            $scope.activeNamespace = gmail_namespace;
+        $scope.message_map = {}; // Actual message cache
+        $scope.activeThread = undefined; // Points to the current active mssage
 
-            $log.info("Getting messages for " + $scope.activeNamespace.name);
-            Wire.rpc('messages_for_folder', [$scope.activeNamespace.id, 'Inbox'], function(data) {
-                    $scope.statustext = "";
+
+        $scope.performSearch = function(query) {
+            if (query.length === 0) {
+                $scope.clearSearch();
+                return;
+            }
+
+            $log.info("Calling search for: " + query);
+            Wire.rpc('search_folder', [query], function(data) {
+
+                var msg_ids = JSON.parse(data);
+                $log.info(["Received msg_ids:", msg_ids]);
+                Wire.rpc('messages_with_ids', [msg_ids], function(data) {
+
                     var arr_from_json = JSON.parse(data);
 
-                    var thread_dict = {};
-                    angular.forEach(arr_from_json, function(value, key) {
 
+                    // THIS IS SHIIIIITY COPYING FROM BELOW
+                    var thread_dict = {};
+                    var freshMessages = [];
+                    angular.forEach(arr_from_json, function(value, key) {
                         var newMessage = new IBMessageMeta(value);
                         $scope.message_map[newMessage.g_id] = newMessage;
 
                         if (!thread_dict[newMessage.g_thrid]) {
-                            thread_dict[newMessage.g_thrid] = new IBThread();
+                            thread_dict[newMessage.g_thrid] = [];
                         }
-                        thread_dict[newMessage.g_thrid].messages.push(newMessage);
+                        thread_dict[newMessage.g_thrid].push(newMessage);
+
+                        freshMessages.push(newMessage);
                     });
+
 
                     /* Below we sort the messages into threads.
                        TODO: This needs to be tested much better.
                      */
+
                     // Sort individual threads in ascending order
-                    angular.forEach(thread_dict, function(thread, key) {
-                        thread.messages.sort(
+                    angular.forEach(thread_dict, function(thread_messages, key) {
+                        thread_messages = thread_messages.sort(
                             function sortDates(msg1, msg2) {
                                 if (msg1.date > msg2.date) return 1;
                                 if (msg1.date < msg2.date) return -1;
@@ -97,82 +109,162 @@ function(
 
                     // Sort threads based on last object (most recent) in descending order
                     all_threads = all_threads.sort(
-                        function sortDates(thread1, thread2) {
-                            if (thread2.recentMessage().date > thread1.recentMessage().date.date) return -1;
-                            if (thread1.recentMessage().date < thread2.recentMessage().date) return 1;
+                        function sortDates(msg_array1, msg_array2) {
+                            if (msg_array1[msg_array1.length - 1].date > msg_array2[msg_array2.length - 1].date) return -1;
+                            if (msg_array1[msg_array1.length - 1].date < msg_array2[msg_array2.length - 1].date) return 1;
                             return 0;
-                    });
+                        });
 
-                    $log.info(all_threads);
-
-                    $scope.threads = all_threads;
+                    $log.info(["All threads:", all_threads]);
                     $scope.displayedThreads = all_threads;
+                });
 
+
+            });
+
+
+        };
+
+
+        $scope.archiveButtonHandler = function() {
+            console.log("Should archive message!");
+
+            var index = $scope.displayedThreads.indexOf($scope.activeThread);
+            if (index > -1) {
+                console.log("Deleting!");
+                $scope.displayedThreads.splice(index, 1);
+
+                $scope.activeThread = null;
+            }
+
+            // $scope.displayedThreads = all_threads;
+        }
+
+
+
+        $scope.clearSearch = function() {
+            console.log("We should clear the search filtering!");
+            $scope.displayedThreads = $scope.threads;
+        };
+
+
+        $scope.loadMessagesForFolder = function(folder_name) {
+
+            // Debug
+            $scope.statustext = "Loading messages...";
+
+            Wire.rpc('messages_for_folder', folder_name, function(data) {
+
+                $scope.statustext = "";
+                var arr_from_json = JSON.parse(data);
+
+                var thread_dict = {};
+                angular.forEach(arr_from_json, function(value, key) {
+
+                    var newMessage = new IBMessageMeta(value);
+                    $scope.message_map[newMessage.g_id] = newMessage;
+
+                    if (!thread_dict[newMessage.g_thrid]) {
+                        thread_dict[newMessage.g_thrid] = new IBThread();
+                    }
+                    thread_dict[newMessage.g_thrid].messages.push(newMessage);
+                });
+
+
+                /* Below we sort the messages into threads.
+                   TODO: This needs to be tested much better.
+                 */
+
+                // Sort individual threads in ascending order
+                angular.forEach(thread_dict, function(thread, key) {
+                    thread.messages.sort(
+                        function sortDates(msg1, msg2) {
+                            if (msg1.date > msg2.date) return 1;
+                            if (msg1.date < msg2.date) return -1;
+                            return 0;
+                        });
+                });
+
+                // Turn dict to array
+                var all_threads = Object.keys(thread_dict).map(function(key) {
+                    return thread_dict[key];
+                });
+
+                // Sort threads based on last object (most recent) in descending order
+                all_threads = all_threads.sort(
+                    function sortDates(thread1, thread2) {
+                        if (thread2.recentMessage().date > thread1.recentMessage().date.date) return -1;
+                        if (thread1.recentMessage().date < thread2.recentMessage().date) return 1;
+                        return 0;
+                });
+
+                console.log(all_threads);
+
+                $scope.threads = all_threads;
+                $scope.displayedThreads = all_threads;
+
+            });
+        };
+
+
+
+        $scope.sendMessage = function(message_string) {
+            Wire.rpc('send_mail', {
+                    message_to_send: {
+                        'subject': 'Hello world',
+                        'body': message_string,
+                        'to': 'christine@spang.cc'
+                    }
+                },
+                function(data) {
+                    alert('Sent mail!');
                 }
             );
 
-        });
-    };
+        };
+
+
+        $scope.openThread = function(selectedThread) {
 
 
 
-
-    $scope.sendMessage = function(message_string) {
-        Wire.rpc('send_mail', {
-                message_to_send: {
-                    'subject': 'Hello world',
-                    'body': message_string,
-                    'to': 'christine@spang.cc'
-                }
-            },
-            function(data) {
-                alert('Sent mail!');
-            }
-        );
-
-    };
+            console.log("SelectedThread:");
+            console.log(selectedThread);
 
 
-    $scope.openThread = function(selectedThread) {
-        $log.info(["SelectedThread:", selectedThread]);
-        $scope.activeThread = selectedThread;
+            $scope.composerActive = false;
+            $scope.activeThread = selectedThread;
+        };
 
-        angular.forEach(selectedThread.messages, function(message, key) {
 
-            Wire.rpc('body_for_messagemeta', [$scope.activeNamespace.id, message.id], function(data) {
-                angular.forEach(JSON.parse(data), function(value, key) {
-                    console.log(value);
-                    message.displayBody = value;
-                });
-
-            });
+        Wire.on('new_mail_notification', function(data) {
+            console.log("new_mail_notificaiton");
+            growl.post("New Message!", "Michael: Lorem ipsum dolor sit amet, consectetur adipisicing");
         });
 
-    };
 
-
-    // Loaded. Load the messages.
-    // TOFIX we should load these once we know the socket has actually connected
-    setTimeout(function() {
-        $scope.loadNamespaces();
-    }, 2000);
+        // Loaded. Load the messages.
+        // TOFIX we should load these once we know the socket has actually connected
+        setTimeout(function() {
+            $scope.loadMessagesForFolder('Inbox');
+        }, 2000);
 
 
 
-    // View stuff
-
-    $scope.isMailViewActive = true;
-    $scope.isTodoViewActive = false;
-    $scope.isStacksViewActive = false;
-    $scope.isPeopleViewActive = false;
-    $scope.isGroupsViewActive = false;
-    $scope.isSettingsViewActive = false;
-
-    $scope.displayTodos = MockData.todos;
+        $scope.isMailViewActive = false;
+        $scope.isTodoViewActive = true;
+        $scope.isStacksViewActive = false;
+        $scope.isPeopleViewActive = false;
+        $scope.isGroupsViewActive = false;
+        $scope.isSettingsViewActive = false;
 
 
-    // For todo sorting
-    $scope.sortableOptions = {
+
+        $scope.displayTodos = MockData.todos;
+
+
+
+      $scope.sortableOptions = {
         revert: false,
         axis: "y",
         // snap: true,
@@ -192,20 +284,21 @@ function(
             };
             console.log(logEntry);
         },
-    };
+
+      };
 
 
-  $scope.makeActive = function() {
-      console.log("Sidebar button clicked!");
-  };
+      $scope.makeActive = function() {
+          console.log("Sidebar button clicked!");
+      };
 
-  $scope.todoCheckboxClickHandler = function(t) {
-      console.log(['Clicked checkbox:', t]);
-  }
+      $scope.todoCheckboxClickHandler = function(t) {
+          console.log(['Clicked checkbox:', t]);
+      }
 
-  $scope.todoRowClickHandler = function(t) {
-      console.log(['Clicked row:', t]);
-  }
+      $scope.todoRowClickHandler = function(t) {
+          console.log(['Clicked row:', t]);
+      }
 
 
 
