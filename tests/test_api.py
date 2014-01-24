445697748c80501d350435ee2485f604d33db0ec
Kavya Joshi <kavya@inboxapp.com>
2014-01-24 21:11:44 +0000

    Post CR changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,27 +1,29 @@
 import zerorpc, pytest
-from . import test_base
+from base import Test
 
 API_SERVER_LOC = 'tcp://0.0.0.0:9999'
 ML_MID = 2
 
 @pytest.fixture(scope='session', autouse=True)
-def test_client(request):
-    test = test_base.Test()
+def api_client(request):
+    test = Test()
     test.client = zerorpc.Client(timeout=5)
     test.client.connect(API_SERVER_LOC)
 
-    request.addfinalizer(test.destroy())
+    def fin():
+    	test.destroy()
+    	request.addfinalizer(fin)
 
     return test.client
 
-# def test_is_mailing_list_message(test_client):
-#     result = test_client.is_mailing_list_message(ML_MID)
-#     assert (result == True)
+def test_is_mailing_list_message(api_client):
+     result = api_client.is_mailing_list_message(ML_MID)
+     assert (result == True)
 
-# def test_mailing_list_info_for_message(test_client):
-#     result = test_client.mailing_list_info_for_message(ML_MID)
+# def test_mailing_list_info_for_message(api_client):
+#     result = api_client.mailing_list_info_for_message(ML_MID)
 #     print result
 
-# def test_headers_for_message(test_client):
-#     result = test_client.headers_for_message()
+# def test_headers_for_message(api_client):
+#     result = api_client.headers_for_message()
 #     print result
