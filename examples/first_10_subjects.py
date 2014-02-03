78517d8c9980ad280b5063f6bf629b6d1b470681
Kavya Joshi <kavya@inboxapp.com>
2014-02-03 15:10:17 -0800

    Tabs->spaces in examples/first_10_subjects.py

--- examples/first_10_subjects.py
+++ examples/first_10_subjects.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
 import zerorpc
 
-# from inbox.server.config import config, load_config
-# load_config()
+from inbox.server.config import config, load_config
+load_config()
 
-# API_SERVER_LOC = config.get('API_SERVER_LOC', None)
-API_SERVER_LOC = 'tcp://0.0.0.0:9999'
-
+API_SERVER_LOC = config.get('API_SERVER_LOC', None)
 
 def get_subjects(n):
-	api_client = zerorpc.Client(timeout=5)
-	api_client.connect(API_SERVER_LOC)
+    api_client = zerorpc.Client(timeout=5)
+    api_client.connect(API_SERVER_LOC)
 
-	subjects = api_client.first_n_subjects(10)
+    subjects = api_client.first_n_subjects(10)
 
-	print """
+    print """
 The first {0} emails in your inbox...
-	""".format(n)
+    """.format(n)
 
-	for s in subjects:
-		print """
-		{0}
-		""".format(s[0])
+    for s in subjects:
+        print """
+        {0}
+        """.format(s[0])
 
 get_subjects(10)
