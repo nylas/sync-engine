63f36285ca59fcd43f591f53c7ffaedf46472347
Christine Spang <spang@inboxapp.com>
2014-02-11 16:23:36 -0800

    pytest fixture for SyncService

---
+++ tests/util/mailsync.py
@@ -0,0 +1,13 @@
+from pytest import fixture
+
+from .base import TestZeroRPC, db
+
+@fixture(scope='session')
+def sync_client(config, db):
+    sync_service_loc = config.get('CRISPIN_SERVER_LOC')
+
+    from inbox.server.mailsync.service import SyncService
+
+    test = TestZeroRPC(config, db, SyncService, sync_service_loc)
+
+    return test.client
