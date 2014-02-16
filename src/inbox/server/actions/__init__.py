8dfef4e8f9a3a2d799cb64383de022895a30d591
Christine Spang <spang@inboxapp.com>
2014-02-16 12:46:04 -0800

    Note about rationale for splitting out write actions

--- src/inbox/server/actions/__init__.py
+++ src/inbox/server/actions/__init__.py
@@ -1,4 +1,10 @@
-""" Code for propagating Inbox datastore changes to the account backend. """
+""" Code for propagating Inbox datastore changes to the account backend.
+
+Dealing with write actions separately from read syncing allows us more
+flexibility in responsiveness/latency on data propagation, and also makes us
+much less likely to royally mess up a sync and e.g. accidentally delete a bunch
+of messages on the account backend because our local datastore is messed up.
+"""
 
 from redis import Redis
 from rq import Queue
