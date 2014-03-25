nylas/sync-engine

commit 5e1141b837afd4fc054cf94a324716104dd09975

Represent raw messages as a namedtuple.
Another fine example of a place where namedtuples lend more clarity to
the code.

src/inbox/server/mailsync/backends/base.py
@@ -111,7 +111,7 @@ def create_db_objects(account_id, db_session, log, folder_name, raw_messages,
     acc = db_session.query(Account).join(Namespace).filter_by(
         id=account_id).one()
     for msg in raw_messages:
-        uid = msg_create_fn(db_session, log, acc, folder_name, *msg)
+        uid = msg_create_fn(db_session, log, acc, folder_name, **msg._asdict())
         if uid is not None:
             new_uids.append(uid)
 

src/inbox/server/crispin.py
@@ -18,6 +18,8 @@ from inbox.util.cache import get_cache, set_cache
 __all__ = ['CrispinClient', 'DummyCrispinClient']
 
 GMetadata = namedtuple('GMetadata', 'msgid thrid')
+RawMessage = namedtuple('RawImapMessage',
+                        'uid internaldate flags body g_thrid g_msgid g_labels')
 
 
 ### main stuff
@@ -343,8 +345,10 @@ class YahooCrispinClient(CrispinClient):
         messages = []
         for uid in sorted(raw_messages.iterkeys(), key=int):
             msg = raw_messages[uid]
-            messages.append((int(uid), msg['INTERNALDATE'], msg['FLAGS'],
-                             msg['BODY[]']))
+            messages.append(RawMessage(uid=int(uid),
+                                       internaldate=msg['INTERNALDATE'],
+                                       flags=msg['FLAGS'],
+                                       body=msg['BODY[]']))
         return messages
 
     def _fetch_uids(self, uids, c):
@@ -457,9 +461,13 @@ class GmailCrispinClient(CrispinClient):
         messages = []
         for uid in sorted(raw_messages.iterkeys(), key=int):
             msg = raw_messages[uid]
-            messages.append((int(uid), msg['INTERNALDATE'], msg['FLAGS'],
-                             msg['BODY[]'], msg['X-GM-THRID'],
-                             msg['X-GM-MSGID'], msg['X-GM-LABELS']))
+            messages.append(RawMessage(uid=int(uid),
+                                       internaldate=msg['INTERNALDATE'],
+                                       flags=msg['FLAGS'],
+                                       body=msg['BODY[]'],
+                                       g_thrid=msg['X-GM-THRID'],
+                                       g_msgid=msg['X-GM-MSGID'],
+                                       g_labels=msg['X-GM-LABELS']))
         return messages
 
     def _fetch_uids(self, uids, c):

src/inbox/server/mailsync/backends/gmail.py
@@ -259,11 +259,13 @@ def download_threads(crispin_client, db_session, log, acc, folder_name,
 
 
 def deduplicate_message_object_creation(account_id, db_session, log,
-        raw_messages):
-    new_g_msgids = {msg[5] for msg in raw_messages}
+                                        raw_messages):
+    log.debug("Deduplicating message object creation")
+    new_g_msgids = {msg.g_msgid for msg in raw_messages}
     existing_g_msgids = set(account.g_msgids(account_id, db_session,
-        in_=new_g_msgids))
-    return [msg for msg in raw_messages if msg[5] not in existing_g_msgids]
+                                             in_=new_g_msgids))
+    return [msg for msg in raw_messages if msg.g_msgid not in
+            existing_g_msgids]
 
 
 def deduplicate_message_download(crispin_client, db_session, log,

src/inbox/server/mailsync/backends/imapaccount.py
@@ -170,14 +170,14 @@ def create_imap_message(db_session, log, account, folder_name, uid,
         return imapuid
 
 
-def add_gmail_attrs(db_session, log, new_uid, flags, folder_name, x_gm_thrid,
-                    x_gm_msgid, x_gm_labels):
+def add_gmail_attrs(db_session, log, new_uid, flags, folder_name, g_thrid,
+                    g_msgid, g_labels):
     """ Gmail-specific post-create-message bits."""
 
-    new_uid.message.g_msgid = x_gm_msgid
+    new_uid.message.g_msgid = g_msgid
     # NOTE: g_thrid == g_msgid on the first message in the thread :)
-    new_uid.message.g_thrid = x_gm_thrid
-    new_uid.update_imap_flags(flags, x_gm_labels)
+    new_uid.message.g_thrid = g_thrid
+    new_uid.update_imap_flags(flags, g_labels)
 
     # NOTE: This code _requires_ autoflush=True, otherwise duplicate
     # threads may attempt to be created and crash.
@@ -187,7 +187,7 @@ def add_gmail_attrs(db_session, log, new_uid, flags, folder_name, x_gm_thrid,
     existing_labels = {l.folder_name.lower() for l in thread.folders}
     # convert things like \Inbox -> Inbox, \Important -> Important
     # also, gmail labels are case-insensitive
-    new_labels = {l.lstrip('\\').lower() for l in x_gm_labels} | \
+    new_labels = {l.lstrip('\\').lower() for l in g_labels} | \
         {folder_name.lower()}
     # remove labels that have been deleted -- note that the \Sent label is
     # per-message, not per-thread, but since we always work at the thread
@@ -210,10 +210,10 @@ def add_gmail_attrs(db_session, log, new_uid, flags, folder_name, x_gm_thrid,
 
 
 def create_gmail_message(db_session, log, account, folder_name, uid,
-                         internaldate, flags, body, x_gm_thrid, x_gm_msgid,
-                         x_gm_labels):
+                         internaldate, flags, body, g_thrid, g_msgid,
+                         g_labels):
     new_uid = create_imap_message(db_session, log, account, folder_name, uid,
                                   internaldate, flags, body)
     if new_uid:
         return add_gmail_attrs(db_session, log, new_uid, flags, folder_name,
-                               x_gm_thrid, x_gm_msgid, x_gm_labels)
+                               g_thrid, g_msgid, g_labels)

commit 41095ebd13bdf592b87fc81ef7432dc5cd99f05a

Don't use args unpacking in call to msg_create_fn.
Summary:
5e1141b837 broke EAS sync because create_eas_message wasn't passing a
namedtuple. I think it's a little clearer to have implementations of
msg_create_fn just take the raw message object and inspect its properties
themselves. This way you can also have the raw message object be any sort of
class, as opposed to a namedtuple specifically.

Test Plan: Ran unit tests; synced test Gmail account.

Reviewers: spang

Reviewed By: spang

Differential Revision: https://review.inboxapp.com/D58

src/inbox/server/mailsync/backends/base.py
@@ -111,7 +111,7 @@ def create_db_objects(account_id, db_session, log, folder_name, raw_messages,
     acc = db_session.query(Account).join(Namespace).filter_by(
         id=account_id).one()
     for msg in raw_messages:
-        uid = msg_create_fn(db_session, log, acc, folder_name, **msg._asdict())
+        uid = msg_create_fn(db_session, log, acc, folder_name, msg)
         if uid is not None:
             new_uids.append(uid)
 

src/inbox/server/mailsync/backends/imap/account.py
@@ -143,8 +143,7 @@ def update_uidvalidity(account_id, session, folder_name, uidvalidity,
     session.add(cached_validity)
 
 
-def create_imap_message(db_session, log, account, folder_name, uid,
-                        internaldate, flags, body):
+def create_imap_message(db_session, log, account, folder_name, msg):
     """ IMAP-specific message creation logic.
 
     This is the one function in this file that gets to take an account
@@ -158,13 +157,13 @@ def create_imap_message(db_session, log, account, folder_name, uid,
         New db object, which links to new Message and Block objects through
         relationships. All new objects are uncommitted.
     """
-    new_msg = create_message(db_session, log, account, uid, folder_name,
-                             internaldate, flags, body)
+    new_msg = create_message(db_session, log, account, msg.uid, folder_name,
+                             msg.internaldate, msg.flags, msg.body)
 
     if new_msg:
         imapuid = ImapUid(imapaccount=account, folder_name=folder_name,
-                          msg_uid=uid, message=new_msg)
-        imapuid.update_imap_flags(flags)
+                          msg_uid=msg.uid, message=new_msg)
+        imapuid.update_imap_flags(msg.flags)
 
         new_msg.is_draft = imapuid.is_draft
         # NOTE: If we're going to make the Inbox datastore API support "read"
@@ -213,13 +212,12 @@ def add_gmail_attrs(db_session, log, new_uid, flags, folder_name, g_thrid,
     return new_uid
 
 
-def create_gmail_message(db_session, log, account, folder_name, uid,
-                         internaldate, flags, body, g_thrid, g_msgid,
-                         g_labels):
+def create_gmail_message(db_session, log, account, folder_name, msg):
     """ Gmail-specific message creation logic. """
 
-    new_uid = create_imap_message(db_session, log, account, folder_name, uid,
-                                  internaldate, flags, body)
+    new_uid = create_imap_message(db_session, log, account, folder_name, msg)
+
     if new_uid:
-        return add_gmail_attrs(db_session, log, new_uid, flags, folder_name,
-                               g_thrid, g_msgid, g_labels)
+        return add_gmail_attrs(db_session, log, new_uid, msg.flags,
+                               folder_name, msg.g_thrid, msg.g_msgid,
+                               msg.g_labels)