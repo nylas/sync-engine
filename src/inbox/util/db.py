f3a54cf4698954370a04dbbc71b4e43c68c4fd66
Kavya Joshi <kavya@inboxapp.com>
2014-04-16 11:13:51 -0700

    util/db.py: Defensive assert for reset_columns default values

--- src/inbox/util/db.py
+++ src/inbox/util/db.py
@@ -34,6 +34,8 @@ def drop_everything(engine, keep_tables=[], reset_columns={}):
                 column_names = reset_columns[table_name]
                 for c in inspector.get_columns(table_name):
                     if c['name'] in column_names:
+                        assert c['default']
+
                         q = "UPDATE {0} SET {1}={2};".\
                             format(table_name, c['name'], c['default'])
                         conn.execute(q)
