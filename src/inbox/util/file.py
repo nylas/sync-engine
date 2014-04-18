cad87726d5bafb141aeaa2b1b205ee3904cb2e5b
Christine Spang <spang@inboxapp.com>
2014-04-18 14:32:33 -0400

    Lint, clean up, and test inbox.util.file, make Lock() gevent-safe.
    Summary:
    This is part of debugging some locking related issues in the sync
    code. Turns out that when I switched the Gmail sync stuff to use a file
    lock, I also totally broke the locking behaviour, because flock() is a
    _process level_ lock, and if you try to grab that lock from multiple
    greenlets, it will happily return and say that you have the lock. This
    modifies the locking to be much more strict; you can only grab the lock
    once, and only from one greenlet, and if you release it more times than
    you grab it it will also throw an exception.
    
    Test Plan: Tests included in tests/general/test_lock.py and
               tests/general/test_misc_utils.py
    
    Reviewers: emfree
    
    Differential Revision: https://review.inboxapp.com/D100

--- src/inbox/util/file.py
+++ src/inbox/util/file.py
@@ -3,12 +3,14 @@ import errno
 import os
 import fcntl
 
+from gevent.coros import BoundedSemaphore
+
+
 def safe_filename(filename):
-    """ Strip potentially bad characters from a filename so it is safe to
-        write to disk.
-    """
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    """ Strip filesystem-unfriendly characters from a filename. """
+    valid_chars = "-_.() {}{}".format(string.ascii_letters, string.digits)
     return ''.join(c for c in filename if c in valid_chars)
+
 
 # http://my.safaribooksonline.com/book/programming/python/0596001673/files/pythoncook-chp-4-sect-16
 def splitall(path):
@@ -18,7 +20,7 @@ def splitall(path):
         if parts[0] == path:  # sentinel for absolute paths
             allparts.insert(0, parts[0])
             break
-        elif parts[1] == path: # sentinel for relative paths
+        elif parts[1] == path:  # sentinel for relative paths
             allparts.insert(0, parts[1])
             break
         else:
@@ -26,46 +28,81 @@ def splitall(path):
             allparts.insert(0, parts[1])
     return allparts
 
+
 def mkdirp(path):
-    """ An equivalent to mkdir -p. This can go away in Python 3.2;
-        just use exists_ok=True.
+    """ An equivalent to mkdir -p.
+
+    This can go away in Python 3.2; just use exists_ok=True.
+
+    Parameters
+    ----------
+    path : str
+        Pathname to create.
     """
     try:
         os.makedirs(path)
     except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:
+            raise
 
-# http://stackoverflow.com/questions/10840533/most-pythonic-way-to-delete-a-file-which-may-not-exist
+
 def remove_file(filename):
+    """ Delete a file and don't raise an error if it doesn't exist.
+
+    From:
+    http://stackoverflow.com/questions/10840533/most-pythonic-way-to-delete-a-file-which-may-not-exist
+    """
     try:
         os.remove(filename)
     except OSError, e:
         if e.errno != errno.ENOENT:
             raise
 
-class Lock:
-    """ UNIX-specific exclusive file locks that are released when the process
-        ends.
 
-        Based on http://blog.vmfarms.com/2011/03/cross-process-locking-and.html,
-        adapted for context managers (the 'with' statement).
+class Lock:
+    """ UNIX-specific exclusive file locks (released when the process ends).
+
+    Based on
+    http://blog.vmfarms.com/2011/03/cross-process-locking-and.html,
+    adapted for context managers (the 'with' statement).
+
+    Modified to be gevent-safe! Locks held by a given Greenlet may not be
+    taken by other Greenlets until released.
+
+    Parameters
+    ----------
+    f : file or str
+        File handle or filename to use as the lock.
+    block : bool
+        Whether to block or throw IOError if the lock is grabbed multiple
+        times.
     """
-    def __init__(self, filename, block=True):
-        self.filename = filename
-        mkdirp(os.path.dirname(filename))
-        self.handle = open(filename, 'w')
+    def __init__(self, f, block=True):
+        if isinstance(f, file):
+            self.filename = f.name
+            self.handle = f if not f.closed else open(f, 'w')
+        else:
+            self.filename = f
+            mkdirp(os.path.dirname(f))
+            self.handle = open(f, 'w')
         if block:
             self.lock_op = fcntl.LOCK_EX
         else:
             self.lock_op = fcntl.LOCK_EX | fcntl.LOCK_NB
+        self.block = block
+        self.gevent_lock = BoundedSemaphore(1)
 
     def acquire(self):
         fcntl.flock(self.handle, self.lock_op)
+        got_gevent_lock = self.gevent_lock.acquire(blocking=self.block)
+        if not got_gevent_lock:
+            raise IOError("Gevent lock is taken")
 
     def release(self):
         fcntl.flock(self.handle, fcntl.LOCK_UN)
+        self.gevent_lock.release()
 
     def __enter__(self):
         self.acquire()
@@ -77,18 +114,21 @@ class Lock:
     def __del__(self):
         self.handle.close()
 
+
 def human_readable_filesize(size_bytes, suffixes=None):
-    """
-    format a size in bytes into a 'human' file size, e.g. bytes, KB, MB, GB, TB, PB
-    Note that bytes/KB will be reported in whole numbers but MB and above will have greater precision
-    e.g. 1 byte, 43 bytes, 443 KB, 4.3 MB, 4.43 GB, etc
+    """ Format a size in bytes into a 'human' file size.
+
+    For example, bytes, KB, MB, GB, TB, PB. Note that bytes/KB will be
+    reported in whole numbers but MB and above will have greater precision e.g.
+    1 byte, 43 bytes, 443 KB, 4.3 MB, 4.43 GB, etc.
     """
     if size_bytes == 1:
         # because I really hate unnecessary plurals
         return "1 byte"
 
     if not suffixes:
-        suffixes = [('bytes',0),('KB',0),('MB',1),('GB',2),('TB',2), ('PB',2)]
+        suffixes = [('bytes', 0), ('KB', 0), ('MB', 1), ('GB', 2), ('TB', 2),
+                    ('PB', 2)]
 
     num = float(size_bytes)
     for suffix, precision in suffixes:
@@ -97,8 +137,8 @@ def human_readable_filesize(size_bytes, suffixes=None):
         num /= 1024.0
 
     if precision == 0:
-        formatted_size = "%d" % num
+        formatted_size = "{:.0f}".format(num)
     else:
         formatted_size = str(round(num, ndigits=precision))
 
-    return "%s %s" % (formatted_size, suffix)
+    return "{} {}".format(formatted_size, suffix)
