5a2847f58703ff0ec62d5e4d9271ca664cc3462b
Christine Spang <spang@inboxapp.com>
2014-05-16 14:55:59 -0700

    Lint inbox.util.html.

--- src/inbox/util/html.py
+++ src/inbox/util/html.py
@@ -13,15 +13,19 @@ except ImportError:
 
 from HTMLParser import HTMLParser
 
+
 # http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
         self.fed = []
+
     def handle_data(self, d):
         self.fed.append(d)
+
     def get_data(self):
         return u''.join(self.fed)
+
 
 def strip_tags(html):
     s = MLStripper()
@@ -30,8 +34,11 @@ def strip_tags(html):
 
 # https://djangosnippets.org/snippets/19/
 re_string = re.compile(ur'(?P<htmlchars>[<&>])|(?P<space>^[ \t]+)|(?P<lineend>\n)|(?P<protocol>(^|\s)((http|ftp)://.*?))(\s|$)', re.S|re.M|re.I|re.U)
+
+
 def plaintext2html(text, tabstop=4):
     assert '\r' not in text, "newlines not normalized"
+
     def do_sub(m):
         c = m.groupdict()
         if c['htmlchars']:
@@ -43,7 +50,7 @@ def plaintext2html(text, tabstop=4):
             t = t.replace(' ', '&nbsp;')
             return t
         elif c['space'] == '\t':
-            return ' '*tabstop;
+            return ' '*tabstop
         else:
             url = m.group('protocol')
             if url.startswith(' '):
@@ -54,9 +61,11 @@ def plaintext2html(text, tabstop=4):
             last = m.groups()[-1]
             if last in ['\n', '\r', '\r\n']:
                 last = '<br>'
-            return u'{0}<a href="{1}">{2}</a>{3}'.format(prefix, url, url, last)
+            return u'{0}<a href="{1}">{2}</a>{3}'.format(
+                prefix, url, url, last)
     return '\n'.join([u'<p>{0}</p>'.format(
         re.sub(re_string, do_sub, p)) for p in text.split('\n\n')])
+
 
 def common_intervals(an, bn):
     """
@@ -65,10 +74,11 @@ def common_intervals(an, bn):
     MIN_LEN = 7
     a = an.split()
     b = bn.split()
+
     def lcs(idx_a, idx_b):
         i = 0
         while idx_a + i < len(a) and idx_b + i < len(b) \
-            and a[idx_a + i] == b[idx_b + i]:
+                and a[idx_a + i] == b[idx_b + i]:
             i += 1
         return i
 
