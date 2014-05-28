7ce678074be1a9b68579bc312f911ec71afd2158
Michael Grinich <mg@inboxapp.com>
2014-05-28 05:16:16 -0700

    add fragment support to url_concat utility

--- src/inbox/util/url.py
+++ src/inbox/util/url.py
@@ -172,15 +172,29 @@ def provider_from_address(email_address):
 
 
 # From tornado.httputil
-def url_concat(url, args):
+def url_concat(url, args, fragments=None):
     """Concatenate url and argument dictionary regardless of whether
     url has existing query parameters.
 
     >>> url_concat("http://example.com/foo?a=b", dict(c="d"))
     'http://example.com/foo?a=b&c=d'
     """
-    if not args:
+
+    if not args and not fragments:
         return url
-    if url[-1] not in ('?', '&'):
-        url += '&' if ('?' in url) else '?'
-    return url + urlencode(args)
+
+    # Strip off hashes
+    while url[-1] == '#':
+        url = url[:-1]
+
+    fragment_tail = ''
+    if fragments:
+        fragment_tail = '#' + urlencode(fragments)
+
+    args_tail = ''
+    if args:
+        if url[-1] not in ('?', '&'):
+            args_tail += '&' if ('?' in url) else '?'
+        args_tail += urlencode(args)
+
+    return url + args_tail + fragment_tail
