c91f5185ec574069fcec3321c64332df4d162155
Eben Freeman <eben@inboxapp.com>
2014-06-09 23:26:31 -0700

    Add decorator to pause and debug sporadic exceptions.
    The thought is that this may be useful for debugging exceptions that occur
    sporadically on staging: manually instrument the offending function with
    `@pause_on_exception`, then when it happens again you can jump into pdb and see
    what's happening.

---
+++ inbox/util/debug.py
@@ -0,0 +1,25 @@
+"""Utilities for debugging failures in development/staging."""
+import pdb
+from inbox.log import log_uncaught_errors
+
+
+def pause_on_exception(exception_type):
+    """Decorator that catches exceptions of type exception_type, logs them, and
+    drops into pdb. Useful for debugging occasional failures.
+
+    Example
+    -------
+    >>> @pause_on_exception(ValueError)
+    ... def bad_function():
+    ...     # Do stuff
+    ...     raise ValueError
+    """
+    def wrapper(func):
+        def wrapped(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception_type:
+                log_uncaught_errors()
+                pdb.set_trace()
+        return wrapped
+    return wrapper
