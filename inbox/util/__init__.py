0c160c8e787a9019571f358b70633efa13cad466
Kavya Joshi <kavya@inboxapp.com>
2014-07-07 14:52:07 -0700

    Support for inbox.util.eas in the /inbox-eas repo; this is where EAS-specific util code would live.
    Summary: As above.
    
    Test Plan: Sync runs as before.
    
    Reviewers: spang
    
    Differential Revision: https://review.inboxapp.com/D226

--- inbox/util/__init__.py
+++ inbox/util/__init__.py
@@ -4,3 +4,6 @@
     Don't add new code here! Find the relevant submodule, or use misc.py if
     there's really no other place.
 """
+# Allow out-of-tree submodules.
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
