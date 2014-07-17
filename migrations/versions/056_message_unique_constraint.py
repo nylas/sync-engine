1387c4af4144e61504b093eb54f7f62521fc6d5c
Christine Spang <spang@inboxapp.com>
2014-07-17 16:35:48 -0700

    [SCHEMA] Remove duplicate Gmail message data and tighten constraints.
    Some bugs caused Message objects to occasionally be duplicated for Gmail
    accounts. I'm not 100% sure we've eradicated them all, but tightening
    the constraints on the table will make it easier to make sure.
    
    Note that MySQL does not apply unique constraints against rows on which
    some of the columns to constraint are NULL, so this does not affect
    non-Gmail accounts (which do not have a g_msgid).

---
+++ migrations/versions/056_message_unique_constraint.py
@@ -0,0 +1,54 @@
+""" Remove duplicated Gmail Message objects and tighten constraints for Gmail messages.
+
+Revision ID: 4b4c5579c083
+Revises: 1925c535a52d
+Create Date: 2014-07-17 00:01:09.410292
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4b4c5579c083'
+down_revision = '4b4674f1a726'
+
+from alembic import op
+from sqlalchemy import func
+
+
+def upgrade():
+    op.drop_constraint('messagecontactassociation',
+                       'messagecontactassociation_ibfk_1', type_='foreignkey')
+    op.drop_constraint('messagecontactassociation',
+                       'messagecontactassociation_ibfk_2', type_='foreignkey')
+    op.create_foreign_key('messagecontactassociation_ibfk_1',
+                          'messagecontactassociation', 'contact',
+                          ['contact_id'], ['id'], ondelete='CASCADE')
+    op.create_foreign_key('messagecontactassociation_ibfk_2',
+                          'messagecontactassociation', 'message',
+                          ['message_id'], ['id'], ondelete='CASCADE')
+    op.drop_constraint('imapuid', 'imapuid_ibfk_2', type_='foreignkey')
+    op.create_foreign_key('imapuid_ibfk_2', 'imapuid', 'message',
+                          ['message_id'], ['id'], ondelete='CASCADE')
+    from inbox.models import Message
+    from inbox.models.session import session_scope
+
+    with session_scope(versioned=False, ignore_soft_deletes=False) \
+            as db_session:
+        groups = db_session.query(
+            Message.id, Message.thread_id, Message.g_msgid)\
+            .filter(~Message.g_msgid.is_(None))\
+            .group_by(Message.thread_id, Message.g_msgid).having(
+                func.count(Message.id) > 1).all()
+
+        for message_id, thread_id, g_msgid in groups:
+            print "deleting duplicates of ({}, {}), saving {}".format(
+                thread_id, g_msgid, message_id)
+            db_session.query(Message).filter(
+                Message.thread_id == thread_id,
+                Message.g_msgid == g_msgid,
+                Message.id != message_id).delete()
+
+    op.execute('ALTER TABLE message ADD UNIQUE INDEX (thread_id, g_msgid)')
+
+
+def downgrade():
+    pass
