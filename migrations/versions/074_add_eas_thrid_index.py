2374d21083b94a136dc5a2c7b3a3e09cfd6df14a
Eben Freeman <eben@inboxapp.com>
2014-08-01 20:05:40 -0700

    [SCHEMA] Add index on eas_thrid.
    For great performance.

---
+++ migrations/versions/074_add_eas_thrid_index.py
@@ -0,0 +1,35 @@
+"""Add eas_thrid index
+
+Revision ID: 3c02d8204335
+Revises: 2525c5245cc2
+Create Date: 2014-08-02 03:12:47.504963
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3c02d8204335'
+down_revision = '2525c5245cc2'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    from inbox.ignition import main_engine
+    engine = main_engine()
+    Base = sa.ext.declarative.declarative_base()
+    Base.metadata.reflect(engine)
+
+    if 'easthread' in Base.metadata.tables:
+        op.create_index('ix_easthread_eas_thrid', 'easthread', ['eas_thrid'],
+                        unique=False, mysql_length=256)
+
+
+def downgrade():
+    from inbox.ignition import main_engine
+    engine = main_engine()
+    Base = sa.ext.declarative.declarative_base()
+    Base.metadata.reflect(engine)
+
+    if 'easthread' in Base.metadata.tables:
+        op.drop_index('ix_easthread_eas_thrid', table_name='easthread')
