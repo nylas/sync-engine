eb376cc45fdaaf1b9ff58f1ce5aac37f812a97a2
Charles Gruenwald <charles@inboxapp.com>
2014-08-04 11:32:04 -0700

    [generic_provider] Support for generic providers via configuration file.
    Summary:
    [alembic] Patch up the rev history.
    
    [providers] Update the providers table.
    
    [sending] Fix a few more bugs.
    
    Fixes T227
    Fixes T250
    
    Test Plan: auth accounts from all providers, make sure you can send / receive mail from each
    
    Reviewers: khamidou, emfree
    
    Reviewed By: emfree
    
    Subscribers: emfree, spang
    
    Maniphest Tasks: T250, T227
    
    Differential Revision: https://review.inboxapp.com/D293

--- migrations/versions/072_generic_providers.py
+++ migrations/versions/072_generic_providers.py
@@ -8,10 +8,11 @@ Create Date: 2014-08-01 14:36:09.980388
 
 # revision identifiers, used by Alembic.
 revision = '43cd2de5ad85'
-down_revision = '3bb5d61c895c'
+down_revision = '4e93522b5b62'
 
 from alembic import op
 import sqlalchemy as sa
+from datetime import datetime
 from inbox.models.session import session_scope
 from sqlalchemy.ext.declarative import declarative_base
 from inbox.ignition import main_engine
@@ -36,38 +37,54 @@ def upgrade():
     class Account(Base):
         __table__ = Base.metadata.tables['account']
 
-    class ImapAccount(Account):
+    class ImapAccount(Base):
         __table__ = Base.metadata.tables['imapaccount']
 
-    class YahooAccount(ImapAccount):
+    class YahooAccount(Base):
         __table__ = Base.metadata.tables['yahooaccount']
 
-    class AOLAccount(ImapAccount):
+    class AOLAccount(Base):
         __table__ = Base.metadata.tables['aolaccount']
 
-    class GenericAccount(ImapAccount):
+    class GenericAccount(Base):
         __table__ = Base.metadata.tables['genericaccount']
+
+    class Secret(Base):
+        __table__ = Base.metadata.tables['secret']
 
     with session_scope(versioned=False, ignore_soft_deletes=False) \
             as db_session:
         for acct in db_session.query(YahooAccount):
-            new_acct = GenericAccount(namespace=acct.namespace,
+            secret = Secret(acl_id=0, type=0, secret=acct.password,
+                            created_at=datetime.utcnow(),
+                            updated_at=datetime.utcnow())
+            db_session.add(secret)
+            db_session.commit()
+
+            new_acct = GenericAccount(id=acct.id,
                                       provider='yahoo')
-            new_acct.email_address = acct.email_address
-            new_acct.password = acct.password
+
+            new_acct.password_id = secret.id
             db_session.add(new_acct)
 
         for acct in db_session.query(AOLAccount):
-            new_acct = GenericAccount(namespace=acct.namespace,
+            secret = Secret(acl_id=0, type=0, secret=acct.password,
+                            created_at=datetime.utcnow(),
+                            updated_at=datetime.utcnow())
+            db_session.add(secret)
+            db_session.commit()
+
+            new_acct = GenericAccount(id=acct.id,
                                       provider='aol')
-            new_acct.email_address = acct.email_address
-            new_acct.password = acct.password
+
+            new_acct.password_id = secret.id
             db_session.add(new_acct)
 
         db_session.commit()
 
-    op.drop_table('aolaccount')
-    op.drop_table('yahooaccount')
+    # don't cascade the delete
+    engine.execute("drop table aolaccount")
+    engine.execute("drop table yahooaccount")
     op.drop_column('imapaccount', 'imap_host')
 
 
@@ -94,31 +111,32 @@ def downgrade():
     class Account(Base):
         __table__ = Base.metadata.tables['account']
 
-    class ImapAccount(Account):
+    class ImapAccount(Base):
         __table__ = Base.metadata.tables['imapaccount']
 
-    class YahooAccount(ImapAccount):
+    class YahooAccount(Base):
         __table__ = Base.metadata.tables['yahooaccount']
 
-    class AOLAccount(ImapAccount):
+    class AOLAccount(Base):
         __table__ = Base.metadata.tables['aolaccount']
 
-    class GenericAccount(ImapAccount):
+    class GenericAccount(Base):
         __table__ = Base.metadata.tables['genericaccount']
 
     with session_scope(versioned=False, ignore_soft_deletes=False) \
             as db_session:
         for acct in db_session.query(GenericAccount):
+            secret = db_session.query(Secret) \
+                .filter_by(id=acct.password_id).one()
+
             if acct.provider == 'yahoo':
                 new_acct = YahooAccount(namespace=acct.namespace,
-                                        password=acct.password,
-                                        email_address=acct.email_address)
+                                        password=secret.secret)
                 db_session.add(new_acct)
             elif acct.provider == 'aol':
                 new_acct = AOLAccount(namespace=acct.namespace,
-                                      password=acct.password,
-                                      email_address=acct.email_address)
+                                      password=secret.secret)
                 db_session.add(new_acct)
         db_session.commit()
 
-    op.drop_table('genericaccount')
+    engine.execute('drop table genericaccount')
