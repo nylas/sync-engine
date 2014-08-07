c3a00a9647111529a53af85c30070980001eb850
Eben Freeman <eben@inboxapp.com>
2014-08-07 11:10:20 -0700

    Revert "Fix alembic version ordering."
    This reverts commit 2c31b304a56d54a1e5febeac0abc02c35cf97208.

--- migrations/versions/075_drop_contacts_search_signals.py
+++ migrations/versions/075_drop_contacts_search_signals.py
@@ -1,14 +1,14 @@
 """drop contacts search signals
 
 Revision ID: 1763103db266
-Revises:3de3979f94bd
+Revises: 3c02d8204335
 Create Date: 2014-08-07 04:50:58.382371
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1763103db266'
-down_revision = '3de3979f94bd'
+down_revision = '3c02d8204335'
 
 from alembic import op
 
