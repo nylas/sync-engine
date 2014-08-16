18280ffab4f5bb6140596fb218c3d53c47ea4efa
Eben Freeman <eben@inboxapp.com>
2014-08-15 19:52:18 -0700

    Handle HTMLParseError when stripping tags.
    Also remove unused code from inbox/util/html.py.

--- inbox/util/html.py
+++ inbox/util/html.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import re
 import cgi
-
+from HTMLParser import HTMLParser, HTMLParseError
 from talon.quotations import (register_xpath_extensions, extract_from_html,
-                              extract_from_plain) # noqa
+                              extract_from_plain)  # noqa
 register_xpath_extensions()
 
-from HTMLParser import HTMLParser
+from inbox.log import get_logger
+
+__all__ = ['strip_tags', 'plaintext2html', 'extract_from_html',
+           'extract_from_plain']
 
 
 # http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
@@ -39,11 +42,14 @@ class MLStripper(HTMLParser):
 
 def strip_tags(html):
     s = MLStripper()
-    s.feed(html)
+    try:
+        s.feed(html)
+    except HTMLParseError:
+        get_logger().error('error stripping tags', raw_html=html)
     return s.get_data()
 
 # https://djangosnippets.org/snippets/19/
-re_string = re.compile(ur'(?P<htmlchars>[<&>])|(?P<space>^[ \t]+)|(?P<lineend>\n)|(?P<protocol>(^|\s)((http|ftp)://.*?))(\s|$)', re.S|re.M|re.I|re.U) # noqa
+re_string = re.compile(ur'(?P<htmlchars>[<&>])|(?P<space>^[ \t]+)|(?P<lineend>\n)|(?P<protocol>(^|\s)((http|ftp)://.*?))(\s|$)', re.S|re.M|re.I|re.U)  # noqa
 
 
 def plaintext2html(text, tabstop=4):
@@ -75,37 +81,3 @@ def plaintext2html(text, tabstop=4):
                 prefix, url, url, last)
     return '\n'.join([u'<p>{0}</p>'.format(
         re.sub(re_string, do_sub, p)) for p in text.split('\n\n')])
-
-
-def common_intervals(an, bn):
-    """
-    finds intervals of common substrings given two strings
-    """
-    MIN_LEN = 7
-    a = an.split()
-    b = bn.split()
-
-    def lcs(idx_a, idx_b):
-        i = 0
-        while idx_a + i < len(a) and idx_b + i < len(b) \
-                and a[idx_a + i] == b[idx_b + i]:
-            i += 1
-        return i
-
-    spans = []
-    start_a, start_b = 0, 0
-
-    while start_a < len(a) - MIN_LEN:
-        while start_b < len(b) - MIN_LEN:
-            l = lcs(start_a, start_b)
-            if l >= MIN_LEN:
-                spans.append((l, start_a, start_b))
-            if l > 0:
-                start_a += l
-                start_b += l
-            else:
-                start_b += 1
-        start_a += 1
-        start_b = 0
-
-    return spans
