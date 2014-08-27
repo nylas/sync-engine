e6dbffb563872e9ab9d2a6c720132801ccb89ceb
Eben Freeman <eben@inboxapp.com>
2014-08-27 10:33:06 -0700

    More efficiently update contacts from messages.
    Summary:
    When we update contacts after syncing a message, it's faster* to do one database
    query to get existing contacts, instead of doing a bunch of queries serially,
    one for each participant in the message.
    
    * faster = ~50-100ms less time for messages with many recipients.
    
    Test Plan: Unit tests added.
    
    Reviewers: khamidou
    
    Reviewed By: khamidou
    
    Differential Revision: https://review.inboxapp.com/D379

--- inbox/contacts/process_mail.py
+++ inbox/contacts/process_mail.py
@@ -1,21 +1,46 @@
 import uuid
+from inbox.util.addr import canonicalize_address as canonicalize
 from inbox.models import Contact, MessageContactAssociation
 
 
 def update_contacts_from_message(db_session, message, account_id):
     with db_session.no_autoflush:
-        for field in ('to_addr', 'from_addr', 'cc_addr', 'bcc_addr'):
-            if getattr(message, field) is None:
-                continue
-            items = set(getattr(message, field))
-            for name, email_address in items:
-                contact = db_session.query(Contact).filter(
-                    Contact.email_address == email_address,
-                    Contact.account_id == account_id).first()
-                if contact is None:
-                    contact = Contact(name=name, email_address=email_address,
+        # First create Contact objects for any email addresses that we haven't
+        # seen yet. We want to dedupe by canonicalized address, so this part is
+        # a bit finicky.
+        canonicalized_addresses = []
+        all_addresses = []
+        for field in (message.from_addr, message.to_addr, message.cc_addr,
+                      message.bcc_addr):
+            # We generally require these attributes to be non-null, but only
+            # set them to the default empty list at flush time. So it's better
+            # to be safe here.
+            if field is not None:
+                all_addresses.extend(field)
+        canonicalized_addresses = [canonicalize(addr) for _, addr in
+                                   all_addresses]
+
+        existing_contacts = db_session.query(Contact).filter(
+            Contact._canonicalized_address.in_(canonicalized_addresses),
+            Contact.account_id == account_id).all()
+
+        contact_map = {c._canonicalized_address: c for c in existing_contacts}
+        for name, email_address in all_addresses:
+            canonicalized_address = canonicalize(email_address)
+            if canonicalized_address not in contact_map:
+                new_contact = Contact(name=name, email_address=email_address,
                                       account_id=account_id, source='local',
                                       provider_name='inbox',
                                       uid=uuid.uuid4().hex)
+                contact_map[canonicalized_address] = new_contact
+
+        # Now associate each contact to the message.
+        for field_name in ('from_addr', 'to_addr', 'cc_addr', 'bcc_addr'):
+            field = getattr(message, field_name)
+            if field is None:
+                continue
+            for name, email_address in field:
+                canonicalized_address = canonicalize(email_address)
+                contact = contact_map.get(canonicalized_address)
                 message.contacts.append(MessageContactAssociation(
-                    contact=contact, field=field))
+                    contact=contact, field=field_name))
---
+++ tests/general/contacts/test_process_mail.py
@@ -0,0 +1,68 @@
+"""Sanity-check our logic for updating contact data from message addressees
+during a sync."""
+from datetime import datetime
+from inbox.models import Message, Thread, Contact
+from inbox.contacts.process_mail import update_contacts_from_message
+
+NAMESPACE_ID = 1
+
+
+def add_fake_message(db_session, thread, from_addr=None, to_addr=None,
+                     cc_addr=None, bcc_addr=None):
+    m = Message()
+    m.from_addr = from_addr or []
+    m.to_addr = to_addr or []
+    m.cc_addr = cc_addr or []
+    m.bcc_addr = bcc_addr or []
+    m.received_date = datetime.utcnow()
+    m.size = 0
+    m.sanitized_body = ''
+    m.snippet = ''
+    m.thread = thread
+    account_id = thread.namespace.account_id
+    update_contacts_from_message(db_session, m, account_id)
+    db_session.add(m)
+    db_session.commit()
+    return m
+
+
+def test_update_contacts_from_message(db):
+    thread = db.session.query(Thread).filter_by(
+        namespace_id=NAMESPACE_ID).first()
+    # Check that only one Contact is created for repeatedly-referenced
+    # addresses.
+    add_fake_message(db.session, thread,
+                     from_addr=[('', 'alpha@example.com')],
+                     cc_addr=[('', 'alpha@example.com')])
+
+    assert db.session.query(Contact).filter_by(
+        email_address='alpha@example.com').count() == 1
+
+    # Check that existing Contacts are used when we process a new message
+    # referencing them.
+    add_fake_message(db.session, thread,
+                     from_addr=[('', 'alpha@example.com')],
+                     cc_addr=[('', 'alpha@example.com')],
+                     to_addr=[('', 'beta@example.com'),
+                              ('', 'gamma@example.com')])
+
+    assert db.session.query(Contact).filter(
+        Contact.email_address.like('%@example.com')).count() == 3
+    alpha = db.session.query(Contact).filter_by(
+        email_address='alpha@example.com').one()
+    assert len(alpha.message_associations) == 4
+
+
+def test_addresses_canonicalized(db):
+    thread = db.session.query(Thread).filter_by(
+        namespace_id=NAMESPACE_ID).first()
+    msg = add_fake_message(db.session, thread,
+                           from_addr=[('', 'alpha.beta@gmail.com')],
+                           cc_addr=[('', 'alphabeta@gmail.com')])
+
+    # Because Gmail addresses with and without periods are the same, check that
+    # there are two MessageContactAssociation instances attached to the message
+    # (one for the from field, one for the cc field), but that that they
+    # reference the same contact.
+    assert len(msg.contacts) == 2
+    assert len(set(association.contact for association in msg.contacts)) == 1
