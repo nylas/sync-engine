c9bd776ab5e103ea530305407d3786e9de28a4ae
Kavya Joshi <kavya@inboxapp.com>
2014-09-03 23:28:11 +0000

    Helper script to re-encrypt secrets.

---
+++ bin/reencrypt.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import os
+
+import nacl.secret
+import nacl.utils
+import yaml
+
+from inbox.config import Configuration
+from inbox.models.secret import Secret
+from inbox.models.session import session_scope
+from inbox.models.util import EncryptionScheme
+
+
+def reencrypt():
+    root_path = '/etc/inboxapp'
+    secrets_path = os.path.join(root_path, 'secrets.yml')
+    with open(secrets_path, 'r') as f:
+        cfg = Configuration(yaml.safe_load(f))
+
+    key = cfg.get_required('SECRET_ENCRYPTION_KEY')
+
+    with session_scope(ignore_soft_deletes=False, versioned=False) as \
+            db_session:
+        secrets = db_session.query(Secret).order_by(Secret.id).all()
+
+        for s in secrets:
+            if not s.encryption_scheme == \
+                    EncryptionScheme.SECRETBOX_WITH_STATIC_KEY:
+                continue
+
+            if isinstance(s._secret, unicode):
+                print '\nUNICODE SECRET!', s.id
+                continue
+            else:
+                encrypted = s._secret
+
+            try:
+                decrypted = nacl.secret.SecretBox(
+                    key=cfg.get_required('SECRET_ENCRYPTION_KEY'),
+                    encoder=nacl.encoding.HexEncoder
+                ).decrypt(
+                    encrypted,
+                    encoder=nacl.encoding.HexEncoder)
+            except TypeError:
+                print '\nTypeError: ', s.id
+                decrypted = nacl.secret.SecretBox(
+                    key=cfg.get_required('SECRET_ENCRYPTION_KEY'),
+                    encoder=nacl.encoding.HexEncoder
+                ).decrypt(encrypted)
+
+            s.secret = decrypted
+
+            db_session.add(s)
+            print '\nReencrypted: ', s.id
+
+        db_session.commit()
+
+
+if __name__ == '__main__':
+    reencrypt()
