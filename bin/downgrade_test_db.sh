ed1362625485e432ef487a094fd09d451f42cd4b
Charles Gruenwald <charles@inboxapp.com>
2014-09-03 20:25:01 -0700

    [bin/test_db] Rename update_test_db.sh script. Add a downgrade_test_db.sh script.

---
+++ bin/downgrade_test_db.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# see setup.sh for info
+color() { printf '\033[%sm%s\033[m\n' "$@"; }
+
+color '36;1' "Loading previous test data..."
+mysql -uroot -proot -Dtest < tests/data/base_dump.sql
+
+color '36;1' "Applying revisions..."
+alembic downgrade $1 --tag=test
+
+color '36;1' "Dumping test data..."
+mysqldump -uroot -proot test > tests/data/base_dump.sql
+
+color '32;1' "Test DB updated successfully."
