nylas/sync-engine

commit 8f0fa83a3bf3a2fdd9120a42e72e9deffac9b791

setup/startup: ensure recent pip; ignore VCS URIs in check_requirements()
Summary:
This will hopefully fix this exception when deploying under Debian
wheezy:

    Traceback (most recent call last):
      File "/srv/inbox/bin/inbox-start", line 70, in <module>
        main()
      File "/usr/local/lib/python2.7/dist-packages/click/core.py", line 488, in __call__
        return self.main(*args, **kwargs)
      File "/usr/local/lib/python2.7/dist-packages/click/core.py", line 474, in main
        self.invoke(ctx)
      File "/usr/local/lib/python2.7/dist-packages/click/core.py", line 659, in invoke
        ctx.invoke(self.callback, **ctx.params)
      File "/usr/local/lib/python2.7/dist-packages/click/core.py", line 325, in invoke
        return callback(*args, **kwargs)
      File "/srv/inbox/bin/inbox-start", line 43, in main
        preflight()
      File "/srv/inbox/inbox/util/startup.py", line 133, in preflight
        check_requirements(requirements_path)
      File "/srv/inbox/inbox/util/startup.py", line 32, in check_requirements
        require(x)
      File "/usr/local/lib/python2.7/dist-packages/pkg_resources.py", line 745, in require
        needed = self.resolve(parse_requirements(requirements))
      File "/usr/local/lib/python2.7/dist-packages/pkg_resources.py", line 603, in resolve
        requirements = list(requirements)[::-1]
      File "/usr/local/lib/python2.7/dist-packages/pkg_resources.py", line 2717, in parse_requirements
        "version spec")
      File "/usr/local/lib/python2.7/dist-packages/pkg_resources.py", line 2682, in scan_list
        raise ValueError(msg, line, "at", line[p:])
    ValueError: ('Expected version spec in', 'git+https://github.com/inboxapp/zerorpc-python.git@master#egg=zerorpc', 'at', '+https://github.com/inboxapp/zerorpc-python.git@master#egg=zerorpc')

Test Plan:
Manually check that bin/inbox-start works either in my dev VM, or we'll just
test it in staging before we deploy.

Reviewers: spang, charles

setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -66,8 +66,11 @@ apt-get -y install git \
                    curl \
                    tnef \
 
+color '35;1' 'Ensuring setuptools and pip versions...'
+easy_install 'setuptools>=5.3' 'pip>=1.5.6'
+hash easy_install pip   # these might be in /usr/local/bin now
+
 color '35;1' 'Installing dependencies from pip...'
-pip install --upgrade setuptools
 pip install -r requirements.txt
 
 pip install -e .

inbox/util/startup.py
@@ -26,7 +26,14 @@ def check_requirements(requirements_path):
     with open(requirements_path, 'r') as f:
         for x in f:
             x = x.strip()
-            if not x or x.startswith('#') or x.startswith('-e '):
+            if not x or x.startswith('#'):
+                # skip blank lines and comments
+                continue
+            elif x.startswith('-e ') or '://' in x:
+                # ignore vcs URIs
+                # XXX(dlitz): we could probably parse `#egg=<version_req>` from
+                # the URI if we wanted to, assuming we want to parse
+                # requirements.txt ourselves at all.
                 continue
             try:
                 require(x)

commit cee8181e8c03406fb14b2d41a675f7dbdd676436

setup.sh: Try harder to upgrade setuptools
Summary:
Ubuntu precise and Debian wheezy come with an old version of
python-setuptools, which is actually an obsolete fork of setuptools
called 'distribute'.  distribute prevents itself from upgrading to the
original setuptools, i.e. "easy_install 'setuptools>=5.3'" is a no-op.

distribute has since been merged back into setuptools, and new versions
of pip are capable of properly upgrading setuptools, so we must upgrade
pip before upgrading setuptools.

It looks like I broke this in 8f0fa83a3bf3a2fdd9120a42e72e9deffac9b791.

Test Plan:
- Fully provisioning from scratch is still broken, but after some manual
  tweaking of unrelated stuff, "easy_install --version" now returns
  "setuptools 5.7".

Reviewers: charles, spang

Reviewed By: spang

Differential Revision: https://review.inboxapp.com/D423

setup.sh
@@ -67,8 +67,11 @@ apt-get -y install git \
                    tnef \
 
 color '35;1' 'Ensuring setuptools and pip versions...'
-easy_install 'setuptools>=5.3' 'pip>=1.5.6'
-hash easy_install pip   # these might be in /usr/local/bin now
+# If python-setuptools is actually the old 'distribute' fork of setuptools,
+# then the first 'pip install setuptools' will be a no-op.
+pip install 'pip>=1.5.6' 'setuptools>=5.3'
+hash pip        # /usr/bin/pip might now be /usr/local/bin/pip
+pip install 'pip>=1.5.6' 'setuptools>=5.3'
 
 color '35;1' 'Installing dependencies from pip...'
 pip install -r requirements.txt