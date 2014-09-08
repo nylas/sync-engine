59dc229880656b647f6c5ca63aa89c27fc542692
Kavya Joshi <kavya@inboxapp.com>
2014-09-08 14:20:12 -0700

    Don't create a new engine in init_db()
    Summary:
    SQLAlchemy engines are lazily initialized so it doesn't have
    any negative effects; it's just unnecessary.
    
    Test Plan: Run create-db, clear-db scripts.
    
    Reviewers: emfree
    
    Differential Revision: https://review.inboxapp.com/D435

--- inbox/ignition.py
+++ inbox/ignition.py
@@ -18,16 +18,17 @@ def main_engine(pool_size=DB_POOL_SIZE, max_overflow=5):
     return engine
 
 
-def init_db():
-    """ Make the tables.
+def init_db(engine):
+    """
+    Make the tables.
 
     This is called only from bin/create-db, which is run during setup.
     Previously we allowed this to run everytime on startup, which broke some
     alembic revisions by creating new tables before a migration was run.
     From now on, we should ony be creating tables+columns via SQLalchemy *once*
     and all subscequent changes done via migration scripts.
+
     """
     from inbox.models.base import MailSyncBase
 
-    engine = main_engine(pool_size=1)
     MailSyncBase.metadata.create_all(engine)
