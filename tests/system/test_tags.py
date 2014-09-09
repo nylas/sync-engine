3fc8d5f03736ede083cc86e39864b2574e9995ae
karim hamidou <khamidou@inboxapp.com>
2014-09-09 18:15:39 +0200

    Test more tagging functionality
    Summary:
    Charles,
    
    I've added two test cases for tagging, one about archiving and the other about marking as spam and trashing.
    The latter crashes the syncback service with a NotImplementedException. I'll look into fixing this.
    
    Could you tell me what you think of the code? Do you have other ideas for tagging test cases?
    
    Test Plan: Ran the tests. Checked that they (mostly) passed.
    
    Reviewers: charles
    
    Reviewed By: charles
    
    Differential Revision: https://review.inboxapp.com/D441

--- tests/system/test_tags.py
+++ tests/system/test_tags.py
@@ -14,7 +14,9 @@ def wait_for_tag(client, thread_id, tagname):
 
 @timeout_loop('tag_remove')
 def wait_for_tag_removal(client, thread_id, tagname):
-    return not wait_for_tag(client, thread_id, tagname)
+    thread = client.threads.find(thread_id)
+    tags = [tag['name'] for tag in thread.tags]
+    return True if tagname not in tags else False
 
 
 @for_all_available_providers
@@ -42,11 +44,59 @@ def test_custom_tag(client):
 
     thread.add_tags([tagname])
     wait_for_tag(client, thread.id, tagname)
+    thread = client.threads.find(thread.id)
 
     thread.remove_tags([tagname])
     wait_for_tag_removal(client, thread.id, tagname)
 
     client.tags.delete(t.id)
+
+
+@for_all_available_providers
+def test_archive_tag(client):
+    thread = random.choice(client.threads.all())
+    thread.add_tags(["archive"])
+    wait_for_tag(client, thread.id, "archive")
+    thread = client.threads.find(thread.id)
+    tags = [tag['name'] for tag in thread.tags]
+    assert 'inbox' not in tags, ("Adding the archive tag should"
+                                 "remove the inbox tag")
+
+    # Remove the archive tag and check it's now back in
+    # Inbox.
+    thread.remove_tags(["archive"])
+    wait_for_tag_removal(client, thread.id, "archive")
+
+    thread = client.threads.find(thread.id)
+    tags = [tag['name'] for tag in thread.tags]
+
+    assert 'archive' not in tags and 'inbox' in tags, ("removing archive tag "
+                                                       "should add inbox tag")
+
+
+@for_all_available_providers
+def test_spam_tag(client):
+    # mark a thread as spam, trash it and check the
+    # spam tag is kept.
+    thread = random.choice(client.threads.all())
+    thread.add_tags(["spam"])
+    wait_for_tag(client, thread.id, "spam")
+
+    thread.add_tags(["trash"])
+    wait_for_tag(client, thread.id, "trash")
+
+    thread = client.threads.find(thread.id)
+    tags = [tag['name'] for tag in thread.tags]
+    assert 'spam' in tags and 'trash' in tags, ("trashing a file should"
+                                                "preserve the spam tag")
+
+    # remove both. Check that the file is back in inbox.
+    thread.remove_tags(["spam", "trash"])
+    wait_for_tag_removal(client, thread.id, "spam")
+    wait_for_tag_removal(client, thread.id, "trash")
+    thread = client.threads.find(thread.id)
+    tags = [tag['name'] for tag in thread.tags]
+    assert 'inbox' in tags, "Thread should be back in Inbox"
 
 
 if __name__ == '__main__':
