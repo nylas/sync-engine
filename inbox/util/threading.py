3fbe0d41f073436cc49fcee5a9b050566f39b9c3
Eben Freeman <eben@inboxapp.com>
2014-09-26 15:00:44 -0700

    Fix TypeError in cleanup_subject() when subjecct is None.

--- inbox/util/threading.py
+++ inbox/util/threading.py
@@ -32,6 +32,8 @@ import re
 def cleanup_subject(subject_str):
     """Clean-up a message subject-line.
     For instance, 'Re: Re: Re: Birthday party' becomes 'Birthday party'"""
+    if subject_str is None:
+        return ''
     cleanup_regexp = "^((Re:|RE:|fwd:|FWD:)\s*)+"
     return re.sub(cleanup_regexp, "", subject_str)
 
