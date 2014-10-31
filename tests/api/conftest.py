7c1dd109cb65cae69ca2567068fa4426b755f310
Eben Freeman <eben@inboxapp.com>
2014-10-30 21:42:33 -0700

    Apply gevent monkey-patching before running tests.
    Fixes test failure.

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,4 +1,8 @@
 """ Fixtures don't go here; see util/base.py and friends. """
+# Monkeypatch first, to prevent "AttributeError: 'module' object has no
+# attribute 'poll'" errors when tests import socket, then monkeypatch.
+from gevent import monkey
+monkey.patch_all(aggressive=False)
 # fixtures that are available by default
 from tests.util.base import config, db, log, absolute_path
 
--- tests/general/conftest.py
+++ tests/general/conftest.py
@@ -1,4 +1,7 @@
 """ Fixtures don't go here; see util/base.py and friends. """
+from gevent import monkey
+monkey.patch_all(aggressive=False)
+
 # fixtures that are available by default
 from tests.util.base import config, db, log, absolute_path
 
