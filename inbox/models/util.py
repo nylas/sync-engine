9e2466046681c64c2f4723609aa870f98b52d476
Kavya Joshi <kavya@inboxapp.com>
2014-11-19 16:30:29 -0800

    Update more attrs in reconcile_message().
    Summary:
    Like full_body, msg.sanitized_body, msg.snippet need to be
    updated when reconciled too.
    
    Test Plan: Tests pass as before.
    
    Reviewers: emfree
    
    Reviewed By: emfree
    
    Differential Revision: https://review.inboxapp.com/D777

--- inbox/models/util.py
+++ inbox/models/util.py
@@ -35,18 +35,26 @@ def threads_for_folder(namespace_id, session, folder_name):
 
 
 def reconcile_message(new_message, session):
-    """Check to see if the (synced) Message instance new_message was originally
+    """
+    Check to see if the (synced) Message instance new_message was originally
     created and sent via the Inbox API (based on the X-Inbox-Uid header. If so,
     update the existing message with new attributes from the synced message
-    and return it."""
+    and return it.
+
+    """
     if new_message.inbox_uid is not None:
         existing_message = session.query(Message).filter(
             Message.namespace_id == new_message.namespace_id,
             Message.inbox_uid == new_message.inbox_uid,
             Message.is_created == True).first()
+
         if existing_message is None:
             return None
+
         existing_message.message_id_header = new_message.message_id_header
         existing_message.full_body = new_message.full_body
+        existing_message.sanitized_body = new_message.sanitized_body
+        existing_message.snippet = new_message.snippet
         existing_message.references = new_message.references
+
         return existing_message
