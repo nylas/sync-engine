nylas/sync-engine

commit e9624a728da404424cdad83fe3371617ca092dc3

Correctly update thread tags in IMAP sync.
Summary:
With this commit, we update a thread's tags by aggregating over all ImapUid
folders and labels to figure out what the tags should be. Previously, we
wouldn't remove the inbox tag if the \Inbox *label* was removed from an All
Mail UID -- only if we synced a deleted Inbox UID. This wouldn't work if the
Inbox folder syncs lagged such that we never actually saw an Inbox UID for the
message.

Fixes T632.

Test Plan:
Run syncs and check that the inbox tag is removed from archived
threads. Automated tests to come, but I want to put this on staging sooner
rather than later.

Reviewers: spang, khamidou

Reviewed By: khamidou

Maniphest Tasks: T632

Differential Revision: https://review.inboxapp.com/D810

inbox/mailsync/backends/imap/common.py
@@ -13,8 +13,7 @@ from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 
 from inbox.contacts.process_mail import update_contacts_from_message
-from inbox.models.message import Message
-from inbox.models.folder import Folder
+from inbox.models import Message, Folder
 from inbox.models.backends.imap import ImapUid, ImapFolderInfo
 from inbox.models.util import reconcile_message
 
@@ -28,84 +27,72 @@ def all_uids(account_id, session, folder_name):
         Folder.name == folder_name)}
 
 
-def update_thread_labels(thread, folder_name, g_labels, db_session):
-    """ Make sure `thread` has all the right labels. """
-    existing_labels = {folder.name.lower() for folder in thread.folders
-                       if folder.name is not None} | \
-                      {folder.canonical_name for folder in thread.folders
-                       if folder.canonical_name is not None}
+def _folders_for_labels(g_labels, account, db_session):
+    """Given a set of Gmail label strings, return the set of associated Folder
+    objects. Creates new (un-added, uncommitted) Folder instances if needed."""
+    labels = {l.lstrip('\\').lower() for l in g_labels}
+    special_folders = {
+        'inbox': account.inbox_folder,
+        'sent': account.sent_folder,
+        'draft': account.drafts_folder,
+        'important': account.important_folder,
+    }
 
-    labels = {l.lstrip('\\').lower() if isinstance(l, unicode)
-              else unicode(l) for l in g_labels if l is not None}
-
-    # FIXME @karim: We should probably do a joinedload or something here.
-    for message in thread.messages:
-        for imapuid in message.imapuids:
-            if imapuid.g_labels is not None:
-                for label in imapuid.g_labels:
-                    if isinstance(label, unicode):
-                        labels.add(label.lstrip('\\').lower())
-                    else:
-                        labels.add(unicode(label))
-
-    labels.add(folder_name.lower())
-
-    # Remove labels that have been deleted -- note that the \Inbox, \Sent,
-    # \Important, \Starred, and \Drafts labels are per-message, not per-thread,
-    # but since we always work at the thread level, _we_ apply the label to the
-    # whole thread.
-    # TODO: properly aggregate \Inbox, \Sent, \Important, and \Drafts
-    # per-message so we can detect deletions properly.
-    folders_to_discard = []
-    for folder in thread.folders:
-        if folder.canonical_name not in ('inbox', 'sent', 'drafts',
-                                         'important', 'starred', 'all'):
-            if folder.lowercase_name not in labels:
-                folders_to_discard.append(folder)
-    for folder in folders_to_discard:
-        thread.folders.discard(folder)
-
-    # add new labels
+    folders = set()
     for label in labels:
-        if label.lower() not in existing_labels:
-            # The problem here is that Gmail's attempt to squash labels and
-            # IMAP folders into the same abstraction doesn't work perfectly. In
-            # particular, there is a '[Gmail]/Sent' folder, but *also* a 'Sent'
-            # label, and so on. We handle this by only maintaining one folder
-            # object that encapsulates both of these. If a Gmail user does not
-            # have these folders enabled via IMAP, we create Folder rows
-            # with no 'name' attribute and fill in the 'name' if the account
-            # is later reconfigured.
-            canonical_labels = {
-                'sent': thread.namespace.account.sent_folder,
-                'draft': thread.namespace.account.drafts_folder,
-                'starred': thread.namespace.account.starred_folder,
-                'important': thread.namespace.account.important_folder}
-            if label in canonical_labels:
-                folder = canonical_labels[label]
-                if folder:
-                    thread.folders.add(folder)
-                else:
-                    folder = Folder.find_or_create(
-                        db_session, thread.namespace.account, None, label)
-                    thread.folders.add(folder)
-            else:
-                folder = Folder.find_or_create(db_session,
-                                               thread.namespace.account, label)
-                thread.folders.add(folder)
-    return labels
+        if label in special_folders:
+            folders.add(special_folders[label])
+        else:
+            folders.add(
+                Folder.find_or_create(db_session, account, label))
+    return folders
+
+
+def add_any_new_thread_labels(thread, new_uid, db_session):
+    """Update the folders associated with a thread when a new uid is synced for
+    it."""
+    thread.folders.add(new_uid.folder)
+    if new_uid.g_labels is not None:
+        folders_for_labels = _folders_for_labels(
+            new_uid.g_labels, thread.namespace.account, db_session)
+        for folder in folders_for_labels:
+            thread.folders.add(folder)
+
+
+def recompute_thread_labels(thread, db_session):
+    """Aggregate folders and labels for a thread's Imapuids, and make sure the
+    thread has the right folders associated with it."""
+    g_labels = set()
+    expected_folders = set()
+    for message in thread.messages:
+        for uid in message.imapuids:
+            if uid.g_labels is not None:
+                g_labels.update(uid.g_labels)
+            expected_folders.add(uid.folder)
+
+    folders_for_labels = _folders_for_labels(
+        g_labels, thread.namespace.account, db_session)
+    expected_folders.update(folders_for_labels)
+
+    for folder in set(thread.folders):
+        if folder not in expected_folders:
+            thread.folders.discard(folder)
+
+    for folder in expected_folders:
+        thread.folders.add(folder)
 
 
 def update_metadata(account_id, session, folder_name, folder_id, uids,
                     new_flags):
     """
-    Update flags (the only metadata that can change).
+    Update flags and labels (the only metadata that can change).
 
     Make sure you're holding a db write lock on the account. (We don't try
     to grab the lock in here in case the caller needs to put higher-level
     functionality in the lock.)
 
     """
+    affected_threads = set()
     if uids:
         for item in session.query(ImapUid). \
                 filter(ImapUid.account_id == account_id,
@@ -114,10 +101,10 @@ def update_metadata(account_id, session, folder_name, folder_id, uids,
                 options(joinedload(ImapUid.message)):
             flags = new_flags[item.msg_uid].flags
             thread = item.message.thread
+            affected_threads.add(thread)
             if hasattr(new_flags[item.msg_uid], 'labels'):  # i.e: gmail
                 labels = new_flags[item.msg_uid].labels
                 item.g_labels = [label for label in labels]
-                update_thread_labels(thread, folder_name, labels, session)
             else:
                 labels = None
             item.update_imap_flags(flags, labels)
@@ -134,6 +121,9 @@ def update_metadata(account_id, session, folder_name, folder_id, uids,
                     thread.apply_tag(unread_tag)
                 elif all(m.is_read for m in thread.messages):
                     thread.remove_tag(unread_tag)
+
+        for thread in affected_threads:
+            recompute_thread_labels(thread, session)
 
 
 def remove_messages(account_id, session, uids, folder):
@@ -177,18 +167,7 @@ def remove_messages(account_id, session, uids, folder):
             if not thread.messages:
                 session.delete(thread)
             else:
-                # We've deleted messages, we've got to update thread.folders.
-                # We use a (temporary) heuristic for this: a thread has
-                # the same folders as its latest message.
-
-                # NB: thread.messages is sorted by date ascending.
-                most_recent_message = thread.messages[-1]
-                mrm_folders = [uid.folder
-                               for uid in most_recent_message.imapuids]
-                thread_folders = [fld for fld in thread.folders]
-                for fld in thread_folders:
-                    if fld not in mrm_folders:
-                        thread.folders.discard(fld)
+                recompute_thread_labels(thread, session)
 
         session.commit()
 

bin/regen-labels
@@ -1,0 +1,51 @@
+#!/usr/bin/env python
+import gevent
+import gevent.monkey
+import gevent.pool
+gevent.monkey.patch_all()
+import click
+from inbox.log import configure_logging, get_logger
+from inbox.models.session import session_scope
+from inbox.models import Namespace
+from inbox.models.backends.imap import ImapAccount, ImapThread
+from inbox.mailsync.backends.imap.common import recompute_thread_labels
+configure_logging(False)
+log = get_logger()
+
+
+def regen_labels_for_namespace(namespace_id):
+    log.info('Regenerating labels for namespace',
+             namespace_id=namespace_id)
+    with session_scope() as db_session:
+        namespace = db_session.query(Namespace).get(namespace_id)
+        account = namespace.account
+        if not isinstance(account, ImapAccount):
+            return
+        threads = db_session.query(ImapThread).filter(
+            ImapThread.namespace_id == namespace_id).all()
+        for index, thread in enumerate(threads):
+            recompute_thread_labels(thread, db_session)
+            log.info('Recomputed labels', thread_id=thread.id,
+                     namespace_id=namespace_id)
+            if index % 100 == 0:
+                db_session.commit()
+        db_session.commit()
+
+
+@click.command()
+@click.option('--namespace_ids')
+def main(namespace_ids):
+    if namespace_ids:
+        ns_ids = [int(ns_id) for ns_id in namespace_ids.split(',')]
+    else:
+        with session_scope() as db_session:
+            ns_ids = [ns.id for ns in db_session.query(Namespace)]
+    pool = gevent.pool.Pool(size=10)
+    for ns_id in ns_ids:
+        pool.add(gevent.spawn(regen_labels_for_namespace, ns_id))
+
+    pool.join()
+
+
+if __name__ == '__main__':
+    main()

inbox/mailsync/backends/gmail.py
@@ -317,10 +317,9 @@ class GmailFolderSyncEngine(CondstoreFolderSyncEngine):
             thread = new_uid.message.thread = ImapThread.from_gmail_message(
                 db_session, new_uid.account.namespace, new_uid.message)
 
-            # make sure this thread has all the correct labels
-            common.update_thread_labels(thread, folder.name, msg.g_labels,
-                                        db_session)
-            return new_uid
+        # make sure this thread has all the correct labels
+        common.add_any_new_thread_labels(thread, new_uid, db_session)
+        return new_uid
 
     def download_and_commit_uids(self, crispin_client, folder_name, uids):
         raw_messages = safe_download(crispin_client, uids)

inbox/mailsync/backends/imap/generic.py
@@ -392,13 +392,12 @@ class FolderSyncEngine(Greenlet):
                 for index, message in enumerate(constructed_thread):
                     message.thread_order = index
 
-            db_session.flush()
-            # Make sure this thread has all the correct labels
-            common.update_thread_labels(new_uid.message.thread, folder.name,
-                                        [folder.canonical_name], db_session)
-            new_uid.update_imap_flags(msg.flags)
-
-            return new_uid
+        db_session.flush()
+        # Make sure this thread has all the correct labels
+        common.add_any_new_thread_labels(new_uid.message.thread, new_uid,
+                                         db_session)
+        new_uid.update_imap_flags(msg.flags)
+        return new_uid
 
     def remove_deleted_uids(self, db_session, local_uids, remote_uids):
         """ Remove imapuid entries that no longer exist on the remote.

tests/general/test_message_deletion.py
@@ -2,7 +2,7 @@ import pytest
 from inbox.mailsync.backends.imap.common import remove_messages
 from inbox.models import Account, Folder, Message, Thread
 from inbox.models.backends.imap import ImapUid
-from sqlalchemy.orm.exc import NoResultFound
+from tests.util.base import add_fake_message, add_fake_thread
 
 
 def test_deletion(db):
@@ -45,31 +45,26 @@ def test_deleting_from_a_message_with_multiple_uids(db):
     # Now check that deleting a imapuid from a message with
     # multiple uids doesn't delete the message itself
     ACCOUNT_ID = 1
-    UID = 380
+    NAMESPACE_ID = 1
 
     account = db.session.query(Account).get(ACCOUNT_ID)
     inbox_folder = account.inbox_folder
-    spam_folder = account.spam_folder
+    sent_folder = account.sent_folder
 
-    old_number_of_imapuids = len(inbox_folder.imapuids)
+    thread = add_fake_thread(db.session, NAMESPACE_ID,)
+    message = add_fake_message(db.session, NAMESPACE_ID, thread)
 
-    imapuid = db.session.query(ImapUid).filter(
-                ImapUid.account_id == ACCOUNT_ID,
-                ImapUid.msg_uid == UID).one()
-
-    message = imapuid.message
-    message_id = message.id
-
-    uid = ImapUid()
-    uid.message = message
-    uid.account = account
-    uid.folder = spam_folder
-    uid.msg_uid = 1337
+    sent_uid = ImapUid(message=message, account=account, folder=sent_folder,
+                       msg_uid=1337)
+    inbox_uid = ImapUid(message=message, account=account, folder=inbox_folder,
+                        msg_uid=2222)
+    db.session.add(sent_uid)
+    db.session.add(inbox_uid)
     db.session.commit()
 
-    remove_messages(ACCOUNT_ID, db.session, [UID], inbox_folder.name)
+    remove_messages(ACCOUNT_ID, db.session, [2222], inbox_folder.name)
 
-    msg = db.session.query(Message).get(message_id)
+    msg = db.session.query(Message).get(message.id)
     assert msg is not None, "the associated message should not have been deleted"
 
     assert len(msg.imapuids) == 1, "the message should have only one imapuid"

tests/general/test_thread_creation.py
@@ -3,20 +3,14 @@ import datetime
 import pytest
 from collections import namedtuple
 from inbox.auth.generic import GenericAuthHandler
+from inbox.models import Folder
+from inbox.models.backends.imap import ImapUid
+from tests.util.base import add_fake_thread, add_fake_message
 
+NAMESPACE_ID = 1
+ACCOUNT_ID = 1
 
-MockMessage = namedtuple('Message', ['subject'])
-MockFolder = namedtuple('Message', ['name', 'canonical_name'])
 MockRawMessage = namedtuple('RawMessage', ['flags'])
-
-
-class MockImapUID(object):
-    def __init__(self, message, account):
-        self.message = message
-        self.account = account
-
-    def update_imap_flags(self, *args, **kwargs):
-        pass
 
 
 @pytest.fixture
@@ -42,8 +36,11 @@ def folder_sync_engine(db, monkeypatch):
 
 
 def test_generic_foldersyncengine(db, folder_sync_engine):
-    message = MockMessage("Golden Gate Park next Sat")
-    imapuid = MockImapUID(message, None)
+    thread = add_fake_thread(db.session, NAMESPACE_ID)
+    message = add_fake_message(db.session, NAMESPACE_ID, thread,
+                               subject="Golden Gate Park next Sat")
+    imapuid = ImapUid(message=message, account_id=ACCOUNT_ID,
+                      msg_uid=2222)
     messages = folder_sync_engine.fetch_similar_threads(db.session, imapuid)
     assert messages == [], ("fetch_similar_threads should "
                             "heed namespace boundaries")
@@ -60,9 +57,12 @@ def test_threading_limit(db, folder_sync_engine, monkeypatch):
         MAX_THREAD_LENGTH)
     namespace_id = folder_sync_engine.namespace_id
     account = db.session.query(Account).get(folder_sync_engine.account_id)
-    folder = MockFolder('inbox', 'inbox')
-    msg = MockRawMessage(None)
-    for _ in range(3 * MAX_THREAD_LENGTH):
+    account.inbox_folder = Folder(account=account,
+                                  name='Inbox',
+                                  canonical_name='inbox')
+    folder = account.inbox_folder
+    msg = MockRawMessage([])
+    for i in range(3 * MAX_THREAD_LENGTH):
         m = Message()
         m.namespace_id = namespace_id
         m.received_date = datetime.datetime.utcnow()
@@ -71,7 +71,8 @@ def test_threading_limit(db, folder_sync_engine, monkeypatch):
         m.sanitized_body = ''
         m.snippet = ''
         m.subject = 'unique subject'
-        uid = MockImapUID(m, account)
+        uid = ImapUid(message=m, account=account, msg_uid=2222 + i,
+                      folder=folder)
         folder_sync_engine.add_message_attrs(db.session, uid, msg, folder)
         db.session.add(m)
         db.session.commit()

commit 4a0aefbc951b286b3de8f69ec3c1788675c14a35

Fix regression in Gmail-special-folder-handling.
This fixes a regression introduced in commit e9624a728, in which syncing Gmail
threads marked e.g. as starred or important would fail if the corresponding
folder was disabled from appearing in IMAP, or not present for some other
reason when we synced folder names.

inbox/mailsync/backends/imap/common.py
@@ -31,17 +31,31 @@ def _folders_for_labels(g_labels, account, db_session):
     """Given a set of Gmail label strings, return the set of associated Folder
     objects. Creates new (un-added, uncommitted) Folder instances if needed."""
     labels = {l.lstrip('\\').lower() for l in g_labels}
+
+    # The problem here is that Gmail's attempt to squash labels and
+    # IMAP folders into the same abstraction doesn't work perfectly. In
+    # particular, there is a '[Gmail]/Sent' folder, but *also* a 'Sent'
+    # label, and so on. We handle this by only maintaining one folder
+    # object that encapsulates both of these. If a Gmail user does not
+    # have these folders enabled via IMAP, we create Folder rows
+    # with no 'name' attribute and fill in the 'name' if the account
+    # is later reconfigured.
     special_folders = {
         'inbox': account.inbox_folder,
         'sent': account.sent_folder,
         'draft': account.drafts_folder,
+        'starred': account.starred_folder,
         'important': account.important_folder,
     }
 
     folders = set()
     for label in labels:
         if label in special_folders:
-            folders.add(special_folders[label])
+            folder = special_folders[label]
+            if folder is None:
+                folder = Folder.find_or_create(db_session, account, None,
+                                               label)
+            folders.add(folder)
         else:
             folders.add(
                 Folder.find_or_create(db_session, account, label))

tests/imap/test_update_metadata.py
@@ -1,5 +1,5 @@
 from inbox.crispin import GmailFlags
-from inbox.models import Thread, Folder
+from inbox.models import Thread, Folder, Account
 from inbox.models.backends.imap import ImapUid
 from inbox.mailsync.backends.imap.common import update_metadata
 from tests.util.base import add_fake_message
@@ -42,3 +42,23 @@ def test_update_metadata(db):
     db.session.commit()
     assert 'some_new_label' in [tag.name for tag in first_thread.tags]
     assert 'some_new_label' in [tag.name for tag in second_thread.tags]
+
+
+def test_gmail_label_sync(db):
+    folder = db.session.query(Folder).filter(
+        Folder.account_id == ACCOUNT_ID,
+        Folder.name == '[Gmail]/All Mail').one()
+    account = db.session.query(Account).get(ACCOUNT_ID)
+    if account.important_folder is not None:
+        db.session.delete(account.important_folder)
+    thread = db.session.query(Thread).get(1)
+    message = add_fake_message(db.session, NAMESPACE_ID, thread)
+    db.session.add(ImapUid(account_id=ACCOUNT_ID, message=message,
+                           msg_uid=22222, folder=folder))
+    db.session.commit()
+
+    new_flags = {22222: GmailFlags((), (u'\\Important', u'\\Starred', u'foo'))}
+    update_metadata(ACCOUNT_ID, db.session, folder.name, folder.id, [22222],
+                    new_flags)
+    thread_tag_names = {tag.name for tag in thread.tags}
+    assert {'important', 'starred', 'foo'}.issubset(thread_tag_names)