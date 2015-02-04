c96e9fc6eb7f3f73f324858d78992dcb07c2ad66
Karim Hamidou <khamidou@gmail.com>
2015-02-04 15:07:20 +0100

    Make Accounts unique
    Summary: This is the sister diff to D1106. Mostly, I added a unique constraint to the `_canonicalized_address` field of the `Account` table. Please note that to do this I had to redefine the field in the `Account` table, otherwise SQLAlchemy crashed (it seems to be the same bug I had when I was working on D972 --- SA doesn't like much extending `__table_args__` in a child class).
    
    Test Plan: Authed and synced accounts. Ran migration.
    
    Reviewers: kav-ya, emfree
    
    Reviewed By: emfree
    
    Projects: #sync-engine
    
    Differential Revision: https://review.inboxapp.com/D1114

---
+++ migrations/versions/131_add_unique_account_constraint.py
@@ -0,0 +1,22 @@
+"""add unique account constraint
+
+Revision ID: 13faec74da45
+Revises: 2b288dc444f
+Create Date: 2015-02-02 13:38:21.725739
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '13faec74da45'
+down_revision = '2b288dc444f'
+
+from alembic import op
+
+
+def upgrade():
+    op.create_unique_constraint('unique_account_address', 'account',
+                                ['_canonicalized_address'])
+
+
+def downgrade():
+    op.drop_constraint('unique_account_address', 'account', type_='unique')
