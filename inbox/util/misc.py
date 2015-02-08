ef6b5ca32a337185c48d6b2afb63d49a428f44c7
Eben Freeman <eben@inboxapp.com>
2015-02-08 13:49:34 -0800

    Fix dt_to_timestamp helper for non-UTC environments.

--- inbox/util/misc.py
+++ inbox/util/misc.py
@@ -93,7 +93,7 @@ def parse_references(references, in_reply_to):
 
 
 def dt_to_timestamp(dt):
-    return int(time.mktime(dt.timetuple()))
+    return int(time.mktime(dt.utctimetuple()))
 
 
 def get_internaldate(date, received):
