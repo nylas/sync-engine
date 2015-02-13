1a1392e889eb29499aef246ae89cca30523e1842
Eben Freeman <eben@inboxapp.com>
2015-02-12 19:38:08 -0800

    Align migrations.

--- migrations/versions/133_add_actionlog_index.py
+++ migrations/versions/133_add_actionlog_index.py
@@ -1,14 +1,14 @@
 """add actionlog index
 
 Revision ID: 39fa82d3168e
-Revises: 4ee8aab06ee
+Revises: 1d93c9f9f506
 Create Date: 2015-02-11 21:14:14.402620
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '39fa82d3168e'
-down_revision = '4ee8aab06ee'
+down_revision = '1d93c9f9f506'
 
 from alembic import op
 
