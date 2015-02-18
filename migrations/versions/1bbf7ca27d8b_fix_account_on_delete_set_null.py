885353fab75dacbecd01ca08dea76ef4517533e3
Karim Hamidou <khamidou@gmail.com>
2015-02-18 10:13:36 -0800

    WIP

---
+++ migrations/versions/1bbf7ca27d8b_fix_account_on_delete_set_null.py
@@ -0,0 +1,24 @@
+"""fix_account_on_delete_set_null
+
+Revision ID: 1bbf7ca27d8b
+Revises: 39fa82d3168e
+Create Date: 2015-02-18 17:34:38.115278
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1bbf7ca27d8b'
+down_revision = '39fa82d3168e'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(text("ALTER TABLE account ADD CONSTRAINT `account_ibfk_10` FOREIGN KEY (`default_calendar_id`) REFERENCES `calendar` (`id`) ON DELETE SET NULL"))
+
+
+def downgrade():
+    conn = op.get_bind()
+    conn.execute(text("ALTER TABLE account DROP CONSTRAINT `account_ibfk_10`"))
