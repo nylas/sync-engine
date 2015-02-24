b13fdc3bbc48d89c675e4144a5b1522f97e30e81
Karim Hamidou <khamidou@gmail.com>
2015-02-24 10:23:06 -0800

    make migrations run faster

--- migrations/versions/2192f61f60d5_fix_calendar_defaults.py
+++ migrations/versions/2192f61f60d5_fix_calendar_defaults.py
@@ -21,7 +21,8 @@ def upgrade():
     conn.execute(text("SET FOREIGN_KEY_CHECKS=0;"))
     conn.execute(text("ALTER TABLE calendar MODIFY namespace_id int(11) NOT NULL;"))
     conn.execute(text("ALTER TABLE calendar MODIFY created_at DATETIME NOT NULL;"))
-    conn.execute(text("ALTER TABLE calendar MODIFY uid varchar(767) CHARACTER SET ASCII NOT NULL;"))
+    # changing charset requires copying the whole table
+    # conn.execute(text("ALTER TABLE calendar MODIFY uid varchar(767) CHARACTER SET ASCII NOT NULL;"))
     conn.execute(text("ALTER TABLE calendar MODIFY provider_name varchar(128);"))
     conn.execute(text("ALTER TABLE calendar DROP FOREIGN KEY `calendar_ibfk_2`"))
     conn.execute(text("ALTER TABLE calendar ADD CONSTRAINT `calendar_ibfk_2` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`id`) ON DELETE CASCADE"))
@@ -33,7 +34,7 @@ def downgrade():
     conn.execute(text("SET FOREIGN_KEY_CHECKS=0;"))
     conn.execute(text("ALTER TABLE calendar MODIFY namespace_id int(11);"))
     conn.execute(text("ALTER TABLE calendar MODIFY created_at DATETIME;"))
-    conn.execute(text("ALTER TABLE calendar MODIFY uid varchar(767);"))
+    # conn.execute(text("ALTER TABLE calendar MODIFY uid varchar(767);"))
     conn.execute(text("ALTER TABLE calendar MODIFY provider_name varchar(64);"))
     conn.execute(text("ALTER TABLE calendar DROP FOREIGN KEY `calendar_ibfk_2`"))
     conn.execute(text("ALTER TABLE calendar ADD CONSTRAINT `calendar_ibfk_2` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`id`)"))
