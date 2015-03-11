e5833f3b0f06a4c251fbc40a87f6095e9e5b60ff
Eben Freeman <eben@inboxapp.com>
2015-03-11 15:05:14 -0700

    Pin SQLAlchemy==0.9.6 in setup.py.
    SQLAlchemy 0.9.9 seems to break
    `from sqlalchemy.ext.declarative import as_declarative`.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "click>=2.4",
         "cpu_affinity>=0.1.0",
         "pyyaml",
-        "SQLAlchemy>=0.9.6",
+        "SQLAlchemy==0.9.6",
         "alembic>=0.6.4",
         "requests>=2.4.3",
         "raven>=5.0.0",
