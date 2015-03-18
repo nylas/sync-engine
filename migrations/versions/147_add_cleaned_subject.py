3cade4b3292394a05bad30bff03b2f135022434e
Karim Hamidou <khamidou@gmail.com>
2015-03-18 09:57:56 -0700

    forgot to add metadata lock timeout

--- migrations/versions/147_add_cleaned_subject.py
+++ migrations/versions/147_add_cleaned_subject.py
@@ -12,9 +12,13 @@ down_revision = 'c77a90d524'
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql import text
 
 
 def upgrade():
+    conn = op.get_bind()
+    conn.execute(text("set @@lock_wait_timeout = 20;"))
+
     op.add_column('thread', sa.Column('_cleaned_subject',
                                       sa.String(length=255), nullable=True))
     op.create_index('ix_cleaned_subject', 'thread',
@@ -22,5 +26,8 @@ def upgrade():
 
 
 def downgrade():
+    conn = op.get_bind()
+    conn.execute(text("set @@lock_wait_timeout = 20;"))
+
     op.drop_index('ix_cleaned_subject', table_name='thread')
     op.drop_column('thread', '_cleaned_subject')
