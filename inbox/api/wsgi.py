30d4803e305ec6b169c217bf0ed6bab199887932
Eben Freeman <eben@inboxapp.com>
2015-03-19 20:14:52 -0700

    Instrument API worker to detect greenlet blocking.
    Summary:
    This is a bit of an experiment. API logs contain intimations of greenlets
    blocking for longer than they should, but we don't have a way to definitively
    tell when that happens. This patch adds basic instrumentation in our worker
    class to detect greenlet blocking.
    
    This is inspired by https://github.com/mozilla-services/mozservices/blob/master/mozsvc/gunicorn_worker.py.
    We spawn a real genuine OS thread, which periodically checks to see if a
    greenlet context switch has happened on the main serving thread since the last
    sample; if not, it logs a warning. This is a little different from the approach
    to similar instrumentation for mailsync in `inbox.util.debug.Tracer`, where we
    check for blocking at context switch time.  We don't need to gather statistics
    here, so just doing periodic sampling minimizes context-switching overhead.
    
    Note that this doesn't work with `bin/inbox-api`; you have to do something like
    ```
    gunicorn --worker-class inbox.api.wsgi.InboxWSGIWorker --logger-class \
    inbox.api.wsgi.GunicornLogger --workers=1 --bind 0.0.0.0:5555 inbox.api.srv:app
    ```
    
    Test Plan:
    Introduce blocking code into API route; check that warning and
    stacktrace are logged.
    
    Reviewers: spang
    
    Reviewed By: spang
    
    Differential Revision: https://review.inboxapp.com/D1323

--- inbox/api/wsgi.py
+++ inbox/api/wsgi.py
@@ -1,7 +1,17 @@
+import sys
+import traceback
+import gevent
+import gevent._threading  # This is a clone of the *real* threading module
 from gevent.pywsgi import WSGIHandler, WSGIServer
+import greenlet
 from gunicorn.workers.ggevent import GeventWorker
 import gunicorn.glogging
 import inbox.log
+from inbox.config import config
+log = inbox.log.get_logger()
+
+# Set to 0 in config to disable altogether.
+MAX_BLOCKING_TIME = config.get('MAX_BLOCKING_TIME', 1.)
 
 
 class InboxWSGIHandler(WSGIHandler):
@@ -22,7 +32,7 @@ class InboxWSGIHandler(WSGIHandler):
             client_address = self.client_address
 
         # client_address is '' when requests are forwarded from nginx via
-        # Unix socket. In that case, replace with a menaingful value
+        # Unix socket. In that case, replace with a meaningful value
         if client_address == '':
             client_address = self.headers.get('X-Forward-For')
         status = getattr(self, 'status', None)
@@ -51,9 +61,48 @@ class InboxWSGIWorker(GeventWorker):
     server_class = WSGIServer
     wsgi_handler = InboxWSGIHandler
 
+    def init_process(self):
+        if MAX_BLOCKING_TIME:
+            # Get a reference to the main thread's hub.
+            self._hub = gevent.hub.get_hub()
+            self._active_greenlet = None
+            self._switch_flag = False
+            self._main_thread_id = gevent._threading.get_ident()
+            greenlet.settrace(self._trace)
+            # Spawn a separate OS thread to periodically check if the active
+            # greenlet on the main thread is blocking.
+            gevent._threading.start_new_thread(self._monitoring_thread, ())
+
+        super(InboxWSGIWorker, self).init_process()
+
+    def _trace(self, event, (origin, target)):
+        self._active_greenlet = target
+        self._switch_flag = True
+
+    def _check_blocking(self):
+        if self._switch_flag is False:
+            active_greenlet = self._active_greenlet
+            if active_greenlet is not None and active_greenlet != self._hub:
+                # greenlet.gr_frame doesn't work on another thread -- we have
+                # to get the main thread's frame.
+                frame = sys._current_frames()[self._main_thread_id]
+                formatted_frame = '\t'.join(traceback.format_stack(frame))
+                log.warning('greenlet blocking', frame=formatted_frame)
+        self._switch_flag = False
+
+    def _monitoring_thread(self):
+        try:
+            while True:
+                self._check_blocking()
+                gevent.sleep(MAX_BLOCKING_TIME)
+        # Swallow exceptions raised during interpreter shutdown.
+        except Exception:
+            if sys is not None:
+                raise
+
 
 class GunicornLogger(gunicorn.glogging.Logger):
     def __init__(self, cfg):
         gunicorn.glogging.Logger.__init__(self, cfg)
         inbox.log.configure_logging(is_prod=True)
-        self.error_log = inbox.log.get_logger()
+        self.error_log = log
