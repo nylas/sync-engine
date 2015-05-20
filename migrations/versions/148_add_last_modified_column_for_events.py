652fceb8f7d22c6cb22a81e4ce048ff8edd34e8b
Thomas Steinacher <tom@eggdrop.ch>
2015-05-19 19:32:17 -0700

    Undo revision changes to previous migration

--- migrations/versions/148_add_last_modified_column_for_events.py
+++ migrations/versions/148_add_last_modified_column_for_events.py
@@ -1,14 +1,14 @@
 """add last_modified column for events
 
 Revision ID: 54dcea22a268
-Revises: 41f957b595fc
+Revises: 486c7fa5b533
 Create Date: 2015-03-16 23:15:55.908307
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '54dcea22a268'
-down_revision = '41f957b595fc'
+down_revision = '486c7fa5b533'
 
 from alembic import op
 from sqlalchemy.sql import text
