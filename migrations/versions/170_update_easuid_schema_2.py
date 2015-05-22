7d1888af39bc427f3cc66f4ece2924128504816e
Eben Freeman <eben@inboxapp.com>
2015-05-22 11:05:44 -0700

    Refine migration.

--- migrations/versions/170_update_easuid_schema_2.py
+++ migrations/versions/170_update_easuid_schema_2.py
@@ -33,13 +33,16 @@ def upgrade():
         max_easuid = db_session.query(sa.func.max(EASUid.id)).scalar()
         if max_easuid is None:
             return
-        for i in range(0, max_easuid, 1000):
+        while True:
             results = db_session.query(EASUid, EASFolderSyncStatus). \
                 join(EASFolderSyncStatus, sa.and_(
                     EASUid.fld_uid == EASFolderSyncStatus.eas_folder_id,
                     EASUid.device_id == EASFolderSyncStatus.device_id,
-                    EASUid.easaccount_id == EASFolderSyncStatus.account_id)). \
-                limit(1000).offset(i)
+                    EASUid.easaccount_id == EASFolderSyncStatus.account_id,
+                    EASUid.easfoldersyncstatus_id.is_(None))). \
+                limit(1000).all()
+            if not results:
+                return
             for easuid, easfoldersyncstatus in results:
                 easuid.easfoldersyncstatus_id = easfoldersyncstatus.id
             db_session.commit()
