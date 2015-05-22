bb3d0a1c68a6320efa6535b1394bbadc62245f80
Karim Hamidou <khamidou@gmail.com>
2015-05-22 17:05:29 +0200

    Ship migrations too.

---
+++ migrations/versions/173_add_owner2.py
@@ -0,0 +1,29 @@
+"""fix owner column length --- it's a varchar(255) on our prod db
+when it should be a varchar(1024)
+
+Revision ID: fd32a69381a
+Revises: 365071c47fa7
+Create Date: 2015-05-11 11:47:01.710610
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'fd32a69381a'
+down_revision = 'd0427f9f3d1'
+
+from alembic import op
+from sqlalchemy.sql import text
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(text("set @@lock_wait_timeout = 20;"))
+    conn.execute(text("ALTER TABLE event ALGORITHM=INPLACE, ADD COLUMN owner2 "
+                      "varchar(1024) DEFAULT NULL"))
+
+
+def downgrade():
+    conn = op.get_bind()
+    conn.execute(text("set @@lock_wait_timeout = 20;"))
+    conn.execute(text("ALTER TABLE event ALGORITHM=INPLACE, "
+                      "DROP COLUMN owner2"))
---
+++ migrations/versions/174_backfill_owner2.py
@@ -0,0 +1,29 @@
+"""backfill owner column data
+
+Revision ID: 4ef055945390
+Revises: fd32a69381a
+Create Date: 2015-05-13 14:04:12.304013
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4ef055945390'
+down_revision = 'fd32a69381a'
+
+from alembic import op
+from sqlalchemy.sql import text
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(text("set @@lock_wait_timeout = 20;"))
+
+    print "Copying data."
+    conn.execute(text("UPDATE event set owner2 = owner where owner2 IS NULL"))
+
+    #print "Dropping old column."
+    #conn.execute(text("ALTER TABLE event DROP COLUMN owner"))
+
+
+def downgrade():
+    raise Exception("Can't undo an UPDATE")
