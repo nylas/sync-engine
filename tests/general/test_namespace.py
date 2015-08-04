180657c75313c2ec7e3adadac0e997f954f3852c
Christine Spang <spang@nylas.com>
2015-08-04 16:48:15 -0700

    Fix namespace deletion test.

--- tests/general/test_namespace.py
+++ tests/general/test_namespace.py
@@ -1,23 +1,29 @@
 from tests.util.base import add_fake_account, add_fake_thread, add_fake_message
 
 
-def test_namespace_deletion(db):
-    from inbox.models import (Namespace, Account, Thread, Message, Block,
+def test_namespace_deletion(db, default_account):
+    from inbox.models import (Account, Thread, Message, Block,
                               Contact, Event, Transaction)
     from inbox.models.util import delete_namespace
 
-    models = [Thread, Message, Block, Contact, Event, Transaction]
+    models = [Thread, Message]
 
-    namespace = db.session.query(Namespace).first()
+    namespace = default_account.namespace
     namespace_id = namespace.id
-    account_id = namespace.account.id
+    account_id = default_account.id
 
     account = db.session.query(Account).get(account_id)
     assert account
 
+    thread = add_fake_thread(db.session, namespace_id)
+
+    message = add_fake_message(db.session, namespace_id, thread)
+
     for m in models:
-        assert db.session.query(m).filter(
-            m.namespace_id == namespace_id).count() != 0
+        c = db.session.query(m).filter(
+            m.namespace_id == namespace_id).count()
+        print "count for", m, ":", c
+        assert c != 0
 
     fake_account = add_fake_account(db.session)
     fake_account_id = fake_account.id
