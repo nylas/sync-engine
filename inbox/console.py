750e378f20b3c746a8f4019fc9861a52a37f8054
Christine Spang <spang@nylas.com>
2015-08-05 14:40:35 -0700

    Also start a database session when starting a console without an email address
    Can't think of a time I wanted a console and NOT a database session.

--- inbox/console.py
+++ inbox/console.py
@@ -41,10 +41,11 @@ def start_console(user_email_address=None):
     # You can also do this with
     # $ python -m imapclient.interact -H <host> -u <user> ...
     # but we want to use our session and crispin so we're not.
-    if user_email_address:
-        user_console(user_email_address)
-    else:
-        IPython.embed()
+    with session_scope() as db_session:
+        if user_email_address:
+            user_console(user_email_address)
+        else:
+            IPython.embed()
 
 
 def start_client_console(user_email_address=None):
