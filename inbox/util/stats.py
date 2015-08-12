0424d39da2fa4f006a086662d317a40ed6f4088d
Rob McQueen <rob@nylas.com>
2015-08-12 13:18:43 -0700

    gevent doesn't handle unicode hostnames. Fix statsd

--- inbox/util/stats.py
+++ inbox/util/stats.py
@@ -3,7 +3,7 @@ import statsd
 from inbox.config import config
 
 statsd_client = statsd.StatsClient(
-    config.get("STATSD_HOST", "localhost"),
+    str(config.get("STATSD_HOST", "localhost")),
     config.get("STATSD_PORT", 8125),
     prefix=config.get("STATSD_PREFIX", "mailsync")
 )
