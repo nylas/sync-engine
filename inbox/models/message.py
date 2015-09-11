16021582530a32fed6b8f81f5405a226d9330d57
Eben Freeman <eben@inboxapp.com>
2015-09-10 17:06:03 -0700

    Catch binascii.Error as message decoding error.
    Raised by following trace:
    ```
    Traceback (most recent call last):
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/util/concurrency.py", line 46, in wrapped
        return func(*args, **kwargs)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/mailsync/backends/imap/generic.py", line 181, in _run_impl
        self.state = self.state_handlers[old_state]()
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/util/concurrency.py", line 46, in wrapped
        return func(*args, **kwargs)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/mailsync/backends/imap/generic.py", line 275, in initial_sync
        self.initial_sync_impl(crispin_client)
    	  File "/usr/share/python/inbox-sync/lib/python2.7/site-packages/inbox/mailsync/backends/gmail.py", line 188, in initial_sync_impl
        self.batch_download_uids(crispin_client, uids, g_metadata)
    	  File "/usr/share/python/inbox-sync/lib/python2.7/site-packages/inbox/mailsync/backends/gmail.py", line 387, in batch_download_uids
        self.download_and_commit_uids(crispin_client, batch)
    	  File "/usr/share/python/inbox-sync/lib/python2.7/site-packages/inbox/mailsync/backends/gmail.py", line 323, in download_and_commit_uids
        msg)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/mailsync/backends/imap/generic.py", line 423, in create_message
        new_uid = common.create_imap_message(db_session, acct, folder, msg)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/mailsync/backends/imap/common.py", line 177, in create_imap_message
        body_string=msg.body)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/models/message.py", line 236, in create_from_synced
        html_parts, plain_parts)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/inbox/models/message.py", line 328, in _parse_mimepart
        if mimepart.body is None:
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/part.py", line 451, in body
        return self._container.body
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/part.py", line 47, in body
        self._load_body()
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/part.py", line 76, in _load_body
        self.stream.read(self.end - self._body_start + 1))
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/part.py", line 578, in decode_body
        return decode_charset(content_type, body)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/part.py", line 594, in decode_charset
        body = charsets.convert_to_unicode(charset, body)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/mime/message/charsets.py", line 13, in convert_to_unicode
        return to_unicode(value, charset=charset)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/utils.py", line 73, in to_unicode
        value = _make_unicode(value, charset)
    	  File "/usr/share/python/inbox-sync/local/lib/python2.7/site-packages/flanker/utils.py", line 59, in _make_unicode
        value = value.decode(charset, "strict")
    	  File "/usr/share/python/inbox-sync/lib/python2.7/encodings/base64_codec.py", line 42, in base64_decode
        output = base64.decodestring(input)
    	  File "/usr/lib/python2.7/base64.py", line 321, in decodestring
        return binascii.a2b_base64(s)
    	Error: Incorrect padding
    ```

--- inbox/models/message.py
+++ inbox/models/message.py
@@ -1,3 +1,4 @@
+import binascii
 import datetime
 import itertools
 from hashlib import sha256
@@ -235,7 +236,7 @@ class Message(MailSyncBase, HasRevisions, HasPublicID):
                     msg._parse_mimepart(mid, mimepart, account.namespace.id,
                                         html_parts, plain_parts)
                 except (mime.DecodingError, AttributeError, RuntimeError,
-                        TypeError) as e:
+                        TypeError, binascii.Error) as e:
                     log.error('Error parsing message MIME parts',
                               folder_name=folder_name, account_id=account.id,
                               error=e)
