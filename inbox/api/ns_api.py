60afc6147b35e792015d94e1da95597d75f4d54b
Christine Spang <spang@nylas.com>
2015-10-04 12:16:16 -0700

    Explicitly specify index name in contacts API.
    MySQL still gets it wrong.

--- inbox/api/ns_api.py
+++ inbox/api/ns_api.py
@@ -704,7 +704,9 @@ def contact_api():
 
     if args['filter']:
         results = results.filter(Contact.email_address == args['filter'])
-    results = results.order_by(asc(Contact.created_at))
+    results = results.with_hint(
+        Contact, 'USE INDEX (ix_contact_ns_uid_provider_name)')\
+        .order_by(asc(Contact.created_at))
 
     if args['view'] == 'count':
         return g.encoder.jsonify({"count": results.scalar()})
