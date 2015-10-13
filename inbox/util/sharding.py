bb9c0d6ecddfecf07bd0d8f96bb618815fdd3d71
Kavya Joshi <kavya@inboxapp.com>
2015-10-13 14:03:26 -0700

    Shard utility functions.
    Summary:
    Concomitant diff to D2161.
    Introduce utility functions to expose shards/ open shards
    so consumers need not be aware of the underlying implementation
    of the engine_manager.
    
    Test Plan: Tested via the consumer functions.
    
    Reviewers: emfree
    
    Reviewed By: emfree
    
    Differential Revision: https://phab.nylas.com/D2162

--- inbox/util/sharding.py
+++ inbox/util/sharding.py
@@ -1,5 +1,38 @@
 import random
+
+
 from inbox.config import config
+from inbox.ignition import engine_manager
+
+
+def get_shards():
+    return engine_manager.engines.keys()
+
+
+def get_open_shards():
+    # Can't use engine_manager.engines here because it does not track
+    # shard state (open/ closed)
+    database_hosts = config.get_required('DATABASE_HOSTS')
+    open_shards = []
+    for host in database_hosts:
+        open_shards.extend(shard['ID'] for shard in host['SHARDS'] if
+                           shard['OPEN'] and not shard.get('DISABLED'))
+
+    return open_shards
+
+
+def get_shard_schemas():
+    # Can't use engine_manager.engines here because it does not track
+    # shard schemas.
+    shard_schemas = {}
+    database_hosts = config.get_required('DATABASE_HOSTS')
+    for host in database_hosts:
+        for shard in host['SHARDS']:
+            if not shard.get('DISABLED'):
+                shard_id = shard['ID']
+                schema_name = shard['SCHEMA_NAME']
+                shard_schemas[shard_id] = schema_name
+    return shard_schemas
 
 
 def generate_open_shard_key():
@@ -8,12 +41,7 @@ def generate_open_shard_key():
     picked at random.
 
     """
-    database_hosts = config.get_required('DATABASE_HOSTS')
-    open_shards = []
-    for host in database_hosts:
-        open_shards.extend(shard['ID'] for shard in host['SHARDS'] if
-                           shard['OPEN'] and not shard.get('DISABLED'))
-
+    open_shards = get_open_shards()
     # TODO[k]: Always pick min()instead?
     shard_id = random.choice(open_shards)
     key = shard_id << 48
