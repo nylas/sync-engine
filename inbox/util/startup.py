11d9a180f9e420fb08c1454b53221d9ed1d1cb64
Menno Smits <menno@freshfoo.com>
2015-11-17 14:57:07 +1300

    Add a TODO about using IMAPClient's normalise_times attribute

--- inbox/util/startup.py
+++ inbox/util/startup.py
@@ -21,6 +21,16 @@ def check_sudo():
         raise Exception("Don't run Inbox as root!")
 
 
+# TODO(menno) - It's good to have all servers use UTC for general
+# sanity, but the IMAPClient concern mentioned in the warning text
+# below can be avoided. When an IMAPClient instance's
+# `normalise_times` attribute is set to False IMAPClient will return
+# unnormalised, timezone-aware timestamps. Changing this is probably
+# non-trival because timezone and non-timezone-aware timestamps don't
+# mix. Some care will be required to ensure that only timezone-aware
+# timestamps are used where they might mix with timestamps originating
+# from IMAPClient.
+
 _TZ_ERROR_TEXT = """
 WARNING!
 
