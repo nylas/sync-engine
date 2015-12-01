6278d34404e34ff070cafd6047432b26a874059f
Kavya Joshi <kavya@inboxapp.com>
2015-12-01 15:40:47 -0800

    Limit the number of Redis connections maintained in the per sync process ConnectionPool. Also set a connection timeout.
    Summary:
    The MAX_CONNECTIONS constant is set by back-calculating from the number of sync processes
    we have; the SOCKET_TIMEOUT has been picked somewhat arbitrarily.
    
    Test Plan:
    Tested manually.
    
    Reviewers: khamidou, emfree
    
    Reviewed By: emfree
    
    Differential Revision: https://phab.nylas.com/D2302

--- inbox/heartbeat/config.py
+++ inbox/heartbeat/config.py
@@ -1,4 +1,4 @@
-from redis import StrictRedis
+from redis import StrictRedis, BlockingConnectionPool
 
 from inbox.config import config
 
@@ -10,9 +10,30 @@ ALIVE_EXPIRY = int(config.get('BASE_ALIVE_THRESHOLD', 480))
 CONTACTS_FOLDER_ID = '-1'
 EVENTS_FOLDER_ID = '-2'
 
+MAX_CONNECTIONS = 100
+WAIT_TIMEOUT = 5
+SOCKET_TIMEOUT = 60
+
+connection_pool = None
+
+
+def _get_connection_pool():
+    # get_redis_client() is called once per sync process at the time of
+    # instantiating the singleton HeartBeatStore, so doing this here
+    # should be okay for now.
+    # TODO[k]: Refactor.
+    global connection_pool
+    if connection_pool is None:
+        connection_pool = BlockingConnectionPool(
+            max_connections=MAX_CONNECTIONS, timeout=WAIT_TIMEOUT,
+            socket_timeout=SOCKET_TIMEOUT)
+    assert connection_pool is not None
+    return connection_pool
+
 
 def get_redis_client(host=None, port=6379, db=STATUS_DATABASE):
     if not host:
         host = str(config.get_required('REDIS_HOSTNAME'))
         port = int(config.get_required('REDIS_PORT'))
-    return StrictRedis(host, port, db)
+    connection_pool = _get_connection_pool()
+    return StrictRedis(host, port, db, connection_pool=connection_pool)
