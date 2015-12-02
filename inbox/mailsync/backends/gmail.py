059dc24ae9c61ad91af0156467413d9ac1bd9079
Karim Hamidou <khamidou@gmail.com>
2015-12-01 18:53:26 -0800

    Sync gmail labels deletes
    Summary:
    It's kind of tricky to detect label deletions because gmail users can exclude some labels from IMAP.
    Because of this we can't do something trivial like `delete(set(old_labels) - set(new_labels))`.
    
    My solution is to delete labels only if
    (1) they don't show up in the output of the `LIST` command
    (2) no messages are associated with them.
    
    This allows us to deal with the various possible edge cases: a hidden label with attached messages, an empty label with no messages, etc.
    
    To implement this I chose to use a two-step algorithm:
    
    1. When listing folders, we get the list of labels which aren't present anymore on the remote server, and mark them as "deleted" by setting `Label.deleted_at`.
    
    2. When updating the categories for a message, we delete labels which were marked as deleted and with no attached messages.
    
    Test Plan:
    Tested by:
    1. creating a new label in gmail, deleting it and checking the API picked it up.
    2. creating a label in gmail, adding messages to it, deleting the label, checking that the API picked it up.
    3. creating a label in gmail, adding messages to it, hiding the label, checking that the API deleted the label.
    
    Reviewers: emfree
    
    Reviewed By: emfree
    
    Maniphest Tasks: T4467
    
    Differential Revision: https://phab.nylas.com/D2270

--- inbox/mailsync/backends/gmail.py
+++ inbox/mailsync/backends/gmail.py
@@ -35,6 +35,7 @@ from inbox.models.session import session_scope
 from inbox.mailsync.backends.imap.generic import FolderSyncEngine
 from inbox.mailsync.backends.imap.monitor import ImapSyncMonitor
 from inbox.mailsync.backends.imap import common
+from inbox.mailsync.gc import LabelRenameHandler
 from inbox.mailsync.backends.base import THROTTLE_COUNT, THROTTLE_WAIT
 log = get_logger()
 
@@ -78,7 +79,7 @@ class GmailSyncMonitor(ImapSyncMonitor):
             Label.account_id == self.account_id,
             Label.deleted_at == None)}
 
-        new_labels = set()
+        current_labels = set()
 
         # Create new labels, folders
         for raw_folder in raw_folders:
@@ -143,11 +144,20 @@ class GmailSyncMonitor(ImapSyncMonitor):
         # We can't delete labels directly because Gmail allows users to hide
         # folders --- we need to check that there's no messages still
         # associated with the label.
-        deleted_labels = old_labels - new_labels
+        deleted_labels = old_labels - current_labels
         for deleted_label in deleted_labels:
             deleted_label.deleted_at = datetime.now()
             cat = deleted_label.category
             cat.deleted_at = datetime.now()
+
+        new_labels = current_labels - old_labels
+        for label in new_labels:
+            rename_handler = LabelRenameHandler(
+                account_id=self.account_id,
+                namespace_id=self.namespace_id,
+                label_name=label.name)
+
+            rename_handler.start()
 
         db_session.commit()
 
