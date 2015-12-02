759511df0babcd2c06cf44047461ea2fcdd5043a
Kavya Joshi <kavya@inboxapp.com>
2015-12-01 20:08:22 -0800

    Instantiate the Redis ConnectionPool with the right host, port, db params.

--- inbox/heartbeat/config.py
+++ inbox/heartbeat/config.py
@@ -14,20 +14,26 @@ MAX_CONNECTIONS = 100
 WAIT_TIMEOUT = 5
 SOCKET_TIMEOUT = 60
 
-connection_pool = None
+
+connection_pool_map = {STATUS_DATABASE: None,
+                       REPORT_DATABASE: None}
 
 
-def _get_connection_pool():
+def _get_connection_pool(host, port, db):
     # get_redis_client() is called once per sync process at the time of
     # instantiating the singleton HeartBeatStore, so doing this here
     # should be okay for now.
     # TODO[k]: Refactor.
-    global connection_pool
+    global connection_pool_map
+
+    connection_pool = connection_pool_map.get(db)
     if connection_pool is None:
         connection_pool = BlockingConnectionPool(
+            host=host, port=port, db=db,
             max_connections=MAX_CONNECTIONS, timeout=WAIT_TIMEOUT,
             socket_timeout=SOCKET_TIMEOUT)
-    assert connection_pool is not None
+        connection_pool_map[db] = connection_pool
+
     return connection_pool
 
 
@@ -35,5 +41,6 @@ def get_redis_client(host=None, port=6379, db=STATUS_DATABASE):
     if not host:
         host = str(config.get_required('REDIS_HOSTNAME'))
         port = int(config.get_required('REDIS_PORT'))
-    connection_pool = _get_connection_pool()
+
+    connection_pool = _get_connection_pool(host, port, db)
     return StrictRedis(host, port, db, connection_pool=connection_pool)
