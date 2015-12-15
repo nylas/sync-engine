a716004231b76edce03b4890d5d7074d5c25cda9
ethanblackburn <ethan@nylas.com>
2015-12-14 18:11:19 -0800

    Fix regex for contact search index

--- inbox/contacts/search.py
+++ inbox/contacts/search.py
@@ -57,11 +57,9 @@ def _strip_non_numeric(phone_number):
 
 # Comprehensive list of all unicode control chars (chars for which
 # unicodedata.category() returns 'Cc'), which cloudsearch will reject
-control_chars_re = re.compile(
-    '[\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12'
-    '\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f\x80\x81\x82\x83'
-    '\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95'
-    '\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f]')
+# Regex taken from Amazon docs:
+# http://docs.aws.amazon.com/cloudsearch/latest/developerguide/preparing-data.html#creating-document-batches
+control_chars_re = re.compile(ur'[^\u0009\u000a\u000d\u0020-\uD7FF\uE000-\uFFFD]')
 
 
 def cloudsearch_contact_repr(contact):
