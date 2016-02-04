aa56d4c9aecdc91a93fd24bf0f9c5a5998ce1d53
Kavya Joshi <kavya@inboxapp.com>
2016-02-03 16:51:26 -0800

    Use shard-specific foreign key columns.
    Summary: `Namespace.id`, `Secret.id` in shard 0 is of type INT(11) not BIGINT(20).
    
    Test Plan: Run against staging.
    
    Reviewers: emfree
    
    Differential Revision: https://phab.nylas.com/D2517
    
    Conflicts:
    	migrations/versions/213_add_metadata_table.py

--- migrations/versions/212_add_columns_for_smtp_imap_specific_auth.py
+++ migrations/versions/212_add_columns_for_smtp_imap_specific_auth.py
@@ -10,7 +10,7 @@ Create Date: 2016-01-29 00:27:08.174534
 revision = '501f6b2fef28'
 down_revision = '31aae1ecb374'
 
-from alembic import op
+from alembic import op, context
 from sqlalchemy.sql import text
 
 
@@ -18,15 +18,25 @@ def upgrade():
     conn = op.get_bind()
     conn.execute(text("set @@lock_wait_timeout = 20;"))
     conn.execute(text("SET FOREIGN_KEY_CHECKS=0;"))
+
     # Add new columns.
     conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_username "
                       "CHAR(255) DEFAULT NULL"))
     conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_username "
                       "CHAR(255) DEFAULT NULL"))
-    conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_password_id "
-                      "BIGINT(20)"))
-    conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_password_id "
-                      "BIGINT(20)"))
+
+    shard_id = int(context.get_x_argument(as_dictionary=True).get('shard_id'))
+    if shard_id == 0:
+        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_password_id "
+                     "INT(11)"))
+        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_password_id "
+                     "INT(11)"))
+    else:
+        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_password_id "
+                          "BIGINT(20)"))
+        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_password_id "
+                          "BIGINT(20)"))
+
     # Add ForeignKey constraints.
     conn.execute(text("ALTER TABLE genericaccount ADD CONSTRAINT "
                       "imap_password_id_ifbk FOREIGN KEY "
