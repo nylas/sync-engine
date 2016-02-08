e81c73474c6b15aa3b00e3c16a98ea5859080744
Kavya Joshi <kavya@inboxapp.com>
2016-02-08 10:20:34 -0800

    Update GenericAccount migration to use a single DDL statement.
    Summary: Use a single ALTER table, which causes a table-copy, rather than six.
    
    Test Plan: Tested locally.
    
    Reviewers: logan
    
    Reviewed By: logan
    
    Differential Revision: https://phab.nylas.com/D2541

--- migrations/versions/212_add_columns_for_smtp_imap_specific_auth.py
+++ migrations/versions/212_add_columns_for_smtp_imap_specific_auth.py
@@ -17,46 +17,31 @@ from sqlalchemy.sql import text
 def upgrade():
     conn = op.get_bind()
     conn.execute(text("set @@lock_wait_timeout = 20;"))
-    conn.execute(text("SET FOREIGN_KEY_CHECKS=0;"))
+    conn.execute(text("set @@foreign_key_checks = 0;"))
 
-    # Add new columns.
-    conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_username "
-                      "CHAR(255) DEFAULT NULL"))
-    conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_username "
-                      "CHAR(255) DEFAULT NULL"))
-
+    # Add new columns + ForeignKey constraints.
     shard_id = int(context.get_x_argument(as_dictionary=True).get('shard_id'))
     if shard_id == 0:
-        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_password_id "
-                     "INT(11)"))
-        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_password_id "
-                     "INT(11)"))
+        conn.execute(text("ALTER TABLE genericaccount "
+                          "ADD COLUMN imap_username CHAR(255) DEFAULT NULL, "
+                          "ADD COLUMN smtp_username CHAR(255) DEFAULT NULL, "
+                          "ADD COLUMN imap_password_id INT(11), "
+                          "ADD COLUMN smtp_password_id INT(11), "
+                          "ADD CONSTRAINT imap_password_id_ifbk FOREIGN KEY "
+                          "(`imap_password_id`) REFERENCES `secret` (`id`), "
+                          "ADD CONSTRAINT smtp_password_id_ifbk FOREIGN KEY "
+                          "(`smtp_password_id`) REFERENCES `secret` (`id`);"))
     else:
-        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN imap_password_id "
-                          "BIGINT(20)"))
-        conn.execute(text("ALTER TABLE genericaccount ADD COLUMN smtp_password_id "
-                          "BIGINT(20)"))
-
-    # Add ForeignKey constraints.
-    conn.execute(text("ALTER TABLE genericaccount ADD CONSTRAINT "
-                      "imap_password_id_ifbk FOREIGN KEY "
-                      "(`imap_password_id`) REFERENCES `secret` (`id`)"))
-    conn.execute(text("ALTER TABLE genericaccount ADD CONSTRAINT "
-                      "smtp_password_id_ifbk FOREIGN KEY "
-                      "(`smtp_password_id`) REFERENCES `secret` (`id`)"))
+        conn.execute(text("ALTER TABLE genericaccount "
+                          "ADD COLUMN imap_username CHAR(255) DEFAULT NULL, "
+                          "ADD COLUMN smtp_username CHAR(255) DEFAULT NULL, "
+                          "ADD COLUMN imap_password_id BIGINT(20), "
+                          "ADD COLUMN smtp_password_id BIGINT(20), "
+                          "ADD CONSTRAINT imap_password_id_ifbk FOREIGN KEY "
+                          "(`imap_password_id`) REFERENCES `secret` (`id`), "
+                          "ADD CONSTRAINT smtp_password_id_ifbk FOREIGN KEY "
+                          "(`smtp_password_id`) REFERENCES `secret` (`id`);"))
 
 
 def downgrade():
-    conn = op.get_bind()
-    conn.execute(text("set @@lock_wait_timeout = 20;"))
-    conn.execute(text("SET FOREIGN_KEY_CHECKS=0;"))
-    conn.execute(text("ALTER TABLE genericaccount DROP CONSTRAINT "
-                      "imap_password_id_ifbk"))
-    conn.execute(text("ALTER TABLE genericaccount DROP CONSTRAINT "
-                      "smtp_password_id_ifbk"))
-    conn.execute(text("ALTER TABLE genericaccount DROP COLUMN imap_username"))
-    conn.execute(text("ALTER TABLE genericaccount DROP COLUMN smtp_username"))
-    conn.execute(text("ALTER TABLE genericaccount DROP COLUMN "
-                      "imap_password_id"))
-    conn.execute(text("ALTER TABLE genericaccount DROP COLUMN "
-                      "smtp_password_id"))
+    pass
