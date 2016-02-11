15b7ca17aa7d8f5ed490544b893a1569c485fbe7
Kavya Joshi <kavya@inboxapp.com>
2016-02-11 10:32:22 -0800

    Per-shard AccountTransaction population script.

--- setup.py
+++ setup.py
@@ -91,7 +91,8 @@ setup(
              'bin/detect-utf7-folders',
              'bin/backfix-utf7-folders',
              'bin/populate-imap-smtp-credentials',
-             'bin/populate-accounttransaction'
+             'bin/populate-accounttransaction',
+             'bin/populate-accounttransaction-for-shard'
              ],
 
     # See:
