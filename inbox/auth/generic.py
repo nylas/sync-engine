470ee1a4f752ff3f6bd47632856ef2c5323f8496
Kavya Joshi <kavya@inboxapp.com>
2016-03-17 18:24:16 -0700

    Reset sync_state='running' on re-authing accounts for sync_state='stopped' as well.

--- inbox/auth/generic.py
+++ inbox/auth/generic.py
@@ -266,8 +266,10 @@ class GenericAuthHandler(AuthHandler):
         # Reset the sync_state to 'running' on a successful re-auth.
         # Necessary for API requests to proceed and an account modify delta to
         # be returned to delta/ streaming clients.
-        account.sync_state = ('running' if account.sync_state in
-                              ('running', 'invalid') else account.sync_state)
+        # NOTE: Setting this does not restart the sync. Sync scheduling occurs
+        # via the sync_should_run bit (set to True in update_account() above).
+        account.sync_state = ('running' if account.sync_state else
+                              account.sync_state)
         return True
 
     def interactive_auth(self, email_address):
