d6ca16db7702696db111d7cab0241dbaa650eb43
Michael Grinich <mg@nylas.com>
2016-03-28 17:15:43 -0700

    Remove bitrotted and crufty scripts

--- setup.py
+++ setup.py
@@ -97,19 +97,6 @@ setup(
     # See:
     # https://pythonhosted.org/setuptools/setuptools.html#dynamic-discovery-of-services-and-plugins
     # https://pythonhosted.org/setuptools/pkg_resources.html#entry-points
-    entry_points={
-        # See https://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation # noqa
-        # 'console_scripts': [
-        #     'inbox-consistency-check = inbox.util.consistency_check.__main__:main',  # noqa
-        # ],
-
-        # See inbox/util/consistency_check/__main__.py
-        'inbox.consistency_check_plugins': [
-            'list=inbox.util.consistency_check.list:ListPlugin',
-            'imap_gm=inbox.util.consistency_check.imap_gm:ImapGmailPlugin',
-            'local_gm=inbox.util.consistency_check.local_gm:LocalGmailPlugin',
-        ],
-    },
     zip_safe=False,
     author="Nylas Team",
     author_email="team@nylas.com",
