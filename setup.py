82b9fb410330e8d8daeb67def8ef85259af8c46d
Kavya Joshi <kavya@inboxapp.com>
2016-03-30 13:54:36 -0700

    Add /bin/correct-autoincrements to setup.py

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setup(
              'bin/populate-sync-queue',
              'bin/delete-marked-accounts',
              'bin/backfix-generic-imap-separators.py',
+             'bin/correct-autoincrements'
              ],
 
     # See:
