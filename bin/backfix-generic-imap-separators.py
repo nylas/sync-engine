e4099e56e1038c613a965a92f025321a6a2fc891
Karim Hamidou <khamidou@gmail.com>
2016-04-11 14:25:43 -0700

    Update the script to run it shard by shard.

--- bin/backfix-generic-imap-separators.py
+++ bin/backfix-generic-imap-separators.py
@@ -7,7 +7,8 @@ from inbox.crispin import connection_pool
 
 from nylas.logging import get_logger, configure_logging
 from inbox.models.backends.generic import GenericAccount
-from inbox.models.session import session_scope, global_session_scope
+from inbox.models.session import (session_scope, global_session_scope,
+                                  session_scope_by_shard_id)
 
 configure_logging()
 log = get_logger(purpose='separator-backfix')
@@ -16,26 +17,36 @@ log = get_logger(purpose='separator-backfix')
 @click.command()
 @click.option('--min-id', type=int, default=None)
 @click.option('--max-id', type=int, default=None)
-def main(min_id, max_id):
+@click.option('--shard-id', type=int, default=None)
+def main(min_id, max_id, shard_id):
     generic_accounts = []
     failed = []
 
-    # Get the list of running Gmail accounts.
-    with global_session_scope() as db_session:
-        generic_accounts = db_session.query(GenericAccount).filter(
-            GenericAccount.sync_state == 'running')
+    if min_id is not None or max_id is not None:
+        # Get the list of running Gmail accounts.
+        with global_session_scope() as db_session:
+            generic_accounts = db_session.query(GenericAccount).filter(
+                GenericAccount.sync_state == 'running')
 
-        if min_id is not None:
-            generic_accounts = generic_accounts.filter(
-                GenericAccount.id > min_id)
+            if min_id is not None:
+                generic_accounts = generic_accounts.filter(
+                    GenericAccount.id > min_id)
 
-        if max_id is not None:
-            generic_accounts = generic_accounts.filter(
-                GenericAccount.id <= max_id)
+            if max_id is not None:
+                generic_accounts = generic_accounts.filter(
+                    GenericAccount.id <= max_id)
 
-        generic_accounts = [acc.id for acc in generic_accounts]
+            generic_accounts = [acc.id for acc in generic_accounts]
 
-        db_session.expunge_all()
+            db_session.expunge_all()
+
+    elif shard_id is not None:
+        with session_scope_by_shard_id(shard_id) as db_session:
+            generic_accounts = db_session.query(GenericAccount).filter(
+                GenericAccount.sync_state == 'running')
+
+            generic_accounts = [acc.id for acc in generic_accounts]
+            db_session.expunge_all()
 
     print "Total accounts: %d" % len(generic_accounts)
 
