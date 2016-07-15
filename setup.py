f8939822f66afbfa95cd0162b4579acac090a47c
Michael Pfister <pfista@gmail.com>
2016-07-15 09:57:55 -0700

    Allow environment variable to set log level
    Test Plan: none
    
    Reviewers: spang
    
    Reviewed By: spang
    
    Subscribers: jenkins
    
    Differential Revision: https://phab.nylas.com/D3098

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 
     install_requires=[
         "gevent>=1.0.1",
-        "nylas-production-python>=0.2.0",
+        "nylas-production-python>=0.2.10",
         "click>=2.4",
         "cpu_affinity>=0.1.0",
         "pyyaml",
