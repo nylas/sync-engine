d380b372d8b48c4c9b1a731ac899a77009f4fcd0
Ben Gotow <bengotow@gmail.com>
2016-08-01 18:28:19 -0700

    Perform all account state changes from the monitor and kill all folders if one exits with an exception

--- inbox/mailsync/backends/imap/monitor.py
+++ inbox/mailsync/backends/imap/monitor.py
@@ -1,7 +1,6 @@
 from gevent import sleep
 from gevent.pool import Group
 from gevent.coros import BoundedSemaphore
-from sqlalchemy.orm import load_only
 from inbox.basicauth import ValidationError
 from nylas.logging import get_logger
 from inbox.crispin import retry_crispin, connection_pool
@@ -27,8 +26,7 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
         Seconds to wait between checking for new folders to sync.
     """
 
-    def __init__(self, account,
-                 heartbeat=1, refresh_frequency=30):
+    def __init__(self, account, heartbeat=1, refresh_frequency=30):
         self.refresh_frequency = refresh_frequency
         self.syncmanager_lock = BoundedSemaphore(1)
         self.saved_remote_folders = None
@@ -36,29 +34,12 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
 
         self.folder_monitors = Group()
         self.delete_handler = None
+        self.exiting = False
 
         BaseMailSyncMonitor.__init__(self, account, heartbeat)
 
     @retry_crispin
-    def prepare_sync(self):
-        """
-        Gets and save Folder objects for folders on the IMAP backend. Returns a
-        list of folder names for the folders we want to sync (in order).
-        """
-        with connection_pool(self.account_id).get() as crispin_client:
-            # Get a fresh list of the folder names from the remote
-            remote_folders = crispin_client.folders()
-            # The folders we should be syncing
-            sync_folders = crispin_client.sync_folders()
-
-        if self.saved_remote_folders != remote_folders:
-            with session_scope(self.namespace_id) as db_session:
-                self.save_folder_names(db_session, remote_folders)
-                self.saved_remote_folders = remote_folders
-
-        return sync_folders
-
-    def save_folder_names(self, db_session, raw_folders):
+    def ensure_folders_synced(self, remote_folders):
         """
         Save the folders present on the remote backend for an account.
 
@@ -77,24 +58,26 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
         comparisons.
 
         """
-        account = db_session.query(Account).get(self.account_id)
-        remote_folder_names = {f.display_name.rstrip()[:MAX_FOLDER_NAME_LENGTH]
-                               for f in raw_folders}
-
-        assert 'inbox' in {f.role for f in raw_folders},\
+        assert 'inbox' in {f.role for f in remote_folders},\
             'Account {} has no detected inbox folder'.\
-            format(account.email_address)
+            format(self.email_address)
 
-        local_folders = {f.name: f for f in db_session.query(Folder).filter(
-                         Folder.account_id == self.account_id)}
+        if self.saved_remote_folders != remote_folders:
+            with session_scope(self.namespace_id) as db_session:
+                self.perform_folder_sync(db_session, remote_folders)
+                self.saved_remote_folders = remote_folders
+
+    def perform_folder_sync(self, db_session, remote_folders):
+        local_folders = db_session.query(Folder).filter(Folder.account_id == self.account_id)
+        local_byname = {f.name: f for f in local_folders}
+
+        remote_byname = {f.display_name.rstrip()[:MAX_FOLDER_NAME_LENGTH]: f for f in remote_folders}
+        deleted_names = set(local_byname) - set(remote_byname)
 
         # Delete folders no longer present on the remote.
         # Note that the folder with canonical_name='inbox' cannot be deleted;
         # remote_folder_names will always contain an entry corresponding to it.
-        discard = set(local_folders) - remote_folder_names
-        for name in discard:
-            log.info('Folder deleted from remote', account_id=self.account_id,
-                     name=name)
+        for name in deleted_names:
             if local_folders[name].category_id is not None:
                 cat = db_session.query(Category).get(
                     local_folders[name].category_id)
@@ -103,9 +86,14 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
             del local_folders[name]
 
         # Create new folders
-        for raw_folder in raw_folders:
-            Folder.find_or_create(db_session, account, raw_folder.display_name,
-                                  raw_folder.role)
+        account = None
+        for name, folder in remote_byname.iteritems():
+            if name in local_byname:
+                continue
+            if not account:
+                account = db_session.query(Account).get(self.account_id)
+            Folder.find_or_create(db_session, account, folder.display_name, folder.role)
+
         # Set the should_run bit for existing folders to True (it's True by
         # default for new ones.)
         for f in local_folders.values():
@@ -114,33 +102,35 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
 
         db_session.commit()
 
-    def start_new_folder_sync_engines(self):
-        running_monitors = {monitor.folder_name: monitor for monitor in
-                            self.folder_monitors}
+    def sync_folders_and_ensure_monitors(self):
+        # Grab info from the db and the IMAP server, ensure that folders are
+        # all up-to-date.
+        with connection_pool(self.account_id).get() as crispin_client:
+            remote_folders = crispin_client.folders()
+            folder_names_to_sync = crispin_client.sync_folders()
 
-        for folder_name in self.prepare_sync():
-            if folder_name in running_monitors:
-                thread = running_monitors[folder_name]
-            else:
-                log.info('Folder sync engine started',
-                         account_id=self.account_id,
-                         folder_name=folder_name)
+        self.ensure_folders_synced(remote_folders)
+
+        monitors_byname = {m.folder_name: m for m in self.folder_monitors}
+
+        for folder_name in folder_names_to_sync:
+            if folder_name not in monitors_byname:
                 thread = self.sync_engine_class(self.account_id,
                                                 self.namespace_id,
                                                 folder_name,
                                                 self.email_address,
                                                 self.provider_name,
                                                 self.syncmanager_lock)
+                thread.link_exception(self.folder_monitor_raised_cb)
+                thread.link_value(self.folder_monitor_finished_cb)
                 self.folder_monitors.start(thread)
 
-            while not thread.state == 'poll' and not thread.ready():
-                sleep(self.heartbeat)
+    def folder_monitor_raised_cb(self, thread):
+        self.stop_sync_with_error(thread.exception, thread.folder_name)
 
-            if thread.ready():
-                log.info('Folder sync engine exited',
-                         account_id=self.account_id,
-                         folder_name=folder_name,
-                         error=thread.exception)
+    def folder_monitor_finished_cb(self, thread):
+        log.info('Greenlet exited gracefully', message=thread.value.message,
+                 account_id=self.account_id)
 
     def start_delete_handler(self):
         if self.delete_handler is None:
@@ -151,18 +141,28 @@ class ImapSyncMonitor(BaseMailSyncMonitor):
                 uid_accessor=lambda m: m.imapuids)
             self.delete_handler.start()
 
+    def stop_sync_with_error(self, exc, folder_name=None):
+        log.error('Sync halting with exception', folder_name=folder_name,
+                  exc_info=True, exc=exc, account_id=self.account_id)
+
+        with session_scope(self.namespace_id) as db_session:
+            account = db_session.query(Account).get(self.account_id)
+            account.disable_sync(reason='Sync Error ({})'.format(folder_name), exception=exc)
+
+            if isinstance(exc, ValidationError):
+                account.mark_invalid(exc.message)
+            else:
+                account.mark_stopped()
+
+        self.folder_monitors.kill(block=False)
+        self.delete_handler.kill(block=False)
+        self.exiting = True
+
     def sync(self):
         try:
             self.start_delete_handler()
-            self.start_new_folder_sync_engines()
-            while True:
+            while not self.exiting:
+                self.sync_folders_and_ensure_monitors()
                 sleep(self.refresh_frequency)
-                self.start_new_folder_sync_engines()
         except ValidationError as exc:
-            log.error(
-                'Error authenticating; stopping sync', exc_info=True,
-                account_id=self.account_id, logstash_tag='mark_invalid')
-            with session_scope(self.namespace_id) as db_session:
-                account = db_session.query(Account).get(self.account_id)
-                account.mark_invalid()
-                account.update_sync_error(str(exc))
+            self.stop_sync_with_error(exc)
