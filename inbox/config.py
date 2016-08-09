42381aef0958e960bd9a8fde4f9e035efa0746d7
Michael Pfister <pfista@gmail.com>
2016-08-09 14:56:45 -0700

    Revert "TLS Improvements"
    This reverts commit f7cb6e8e413b659186c371b68fbbd03cdfa62341.

--- inbox/config.py
+++ inbox/config.py
@@ -2,18 +2,6 @@ import errno
 import os
 import yaml
 
-# TODO[mike]: This should be removed once we've updated python to 2.7.9
-# This tells urllib3 to use pyopenssl, which has the latest tls protocols and is
-# more secure than the default python ssl module in python 2.7.4
-import urllib3.contrib.pyopenssl
-urllib3.contrib.pyopenssl.inject_into_urllib3()
-
-# TODO[mike]: This shold be removed once we've updated our base OS. openssl 1.0.1 doesn't support cross-signed certs
-# https://github.com/certifi/python-certifi/issues/26#issuecomment-138322515
-import certifi
-os.environ["REQUESTS_CA_BUNDLE"] = certifi.old_where()
-
-
 __all__ = ['config']
 
 
